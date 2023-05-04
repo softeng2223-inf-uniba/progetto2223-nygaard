561bd708e03a8b1516f5ca4cba91364305b4fb30
mattiacurri <mattia2002@gmail.com>
2023-05-05 00:41:26 +0200

    Impostata base del main

--- src/main/java/it/uniba/app/App.java
+++ src/main/java/it/uniba/app/App.java
@@ -1,5 +1,7 @@
 package it.uniba.app;
 
+import java.util.Scanner;
+
 /**
  * Main class of the application.
  */
@@ -11,6 +13,28 @@ public final class App {
      * @param args command line arguments
      */
     public static void main(final String[] args) {
-
+        Scanner in = new Scanner(System.in);
+        String command;
+        // Creazione del match
+        if (args.length > 0 && (args[0].equals("--help") || args[0].equals("-h"))) {
+            // Stampa help
+        }
+        while (true) {
+            System.out.print("Inserire un comando: ");
+            command = in.nextLine().toLowerCase();
+            switch (command) {
+                /* case "/help" -> Stampa help
+                case "/esci" -> Esegui comando di uscita
+                case "/facile" -> Setta la difficolta a facile
+                case "/medio" -> Setta la difficolta a medio
+                case "/difficile" -> Setta la difficolta a difficile
+                case "/mostralivello" -> Mostrare il livello
+                case "/mostranavi" -> Mostrare le navi e il numero di occorrenze di esse
+                case "/gioca" -> Incomincia la partita
+                case "/svelagriglia" -> Svela la griglia con tanto di navi
+                 */
+                default -> System.out.println("Comando non riconosciuto");
+            }
+        }
     }
 }
