a38fa6ded7411d399070ee7bf22d15cc0030644c
mattiacurri <mattia2002@gmail.com>
2023-05-05 00:36:24 +0200

    Eliminazione esempio di main

--- src/main/java/it/uniba/app/App.java
+++ src/main/java/it/uniba/app/App.java
@@ -6,20 +6,11 @@ package it.uniba.app;
 public final class App {
 
     /**
-     * Get a greeting sentence.
-     *
-     * @return the "Hello World!" string.
-     */
-    public String getGreeting() {
-        return "Hello World!!!";
-    }
-
-    /**
      * Entrypoint of the application.
      *
      * @param args command line arguments
      */
     public static void main(final String[] args) {
-        System.out.println(new App().getGreeting());
+
     }
 }
