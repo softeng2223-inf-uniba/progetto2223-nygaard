b8e6f4bdf4c5cd79686fc71ba539959128b4f355
Emanuele Fontana <fontanaemanuele14@gmail.com>
2023-05-07 15:59:31 +0200

    Definito costruttore Ho definito il costruttore della classe Match inizializzando gli attributi per la gestione della difficoltà di gioco

--- src/main/java/it/uniba/nygaard/game/Match.java
+++ src/main/java/it/uniba/nygaard/game/Match.java
@@ -3,4 +3,10 @@ package it.uniba.nygaard.game;
 public class Match {
   private boolean inGame;
   private int difficuly;
+
+  Match()
+  {
+    this.inGame=false;
+    this.difficuly=Util.DIFFICULTY_NOT_SETTED;
+  }
 }
