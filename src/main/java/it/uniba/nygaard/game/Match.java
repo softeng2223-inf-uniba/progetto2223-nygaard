5aba16e812484aa7bfe3aecbac416f0f7037a292
Emanuele Fontana <fontanaemanuele14@gmail.com>
2023-05-07 16:15:40 +0200

    Modifica attributi Ho aggiunto degli attributi per gestire i nomi delle difficoltà e i tentativi

--- src/main/java/it/uniba/nygaard/game/Match.java
+++ src/main/java/it/uniba/nygaard/game/Match.java
@@ -3,11 +3,16 @@ package it.uniba.nygaard.game;
 public class Match {
   private boolean inGame;
   private int difficulty;
+  private String[] difficultyNames;
+
+  private final int[] attempts;
 
   Match()
   {
     this.inGame=false;
     this.difficulty=Util.DIFFICULTY_NOT_SETTED;
+    this.difficultyNames=new String[]{Util.EASY_NAME,Util.MEDIUM_NAME,Util.HARD_NAME};
+    this.attempts = new int[]{Util.EASY_ATTEMPTS, Util.MEDIUM_ATTEMPTS, Util.HARD_ATTEMPTS};
   }
 
   void setDifficuly(int difficulty)
--- src/main/java/it/uniba/nygaard/game/Util.java
+++ src/main/java/it/uniba/nygaard/game/Util.java
@@ -14,4 +14,7 @@ public class Util {
   public static final int EASY_ATTEMPTS = 50;
   public static final int MEDIUM_ATTEMPTS = 30;
   public static final int HARD_ATTEMPTS = 10;
+  public static final String EASY_NAME="Facile";
+  public static final String MEDIUM_NAME="Medio";
+  public static final String HARD_NAME="Difficile";
 }
