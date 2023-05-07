7771633fba53e15a5eab4d2cf860dcfe2d14cb35
Emanuele Fontana <fontanaemanuele14@gmail.com>
2023-05-07 16:03:09 +0200

    Aggiunto metodo setDifficulty Ho aggiunto il metodo setDifficulty per impostare la difficoltà della partita

--- src/main/java/it/uniba/nygaard/game/Match.java
+++ src/main/java/it/uniba/nygaard/game/Match.java
@@ -2,11 +2,23 @@ package it.uniba.nygaard.game;
 
 public class Match {
   private boolean inGame;
-  private int difficuly;
+  private int difficulty;
 
   Match()
   {
     this.inGame=false;
-    this.difficuly=Util.DIFFICULTY_NOT_SETTED;
+    this.difficulty=Util.DIFFICULTY_NOT_SETTED;
+  }
+
+  void setDifficuly(int difficulty)
+  {
+    if (this.inGame) {
+      System.out.println("Non puoi cambiare difficoltà durante una partita");
+      return;
+    }
+    if (this.difficulty==difficulty){
+      System.out.println("Hai già impostato questa difficoltà");
+      return;
+    }
   }
 }
