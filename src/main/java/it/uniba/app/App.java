6d60021fce872e14efcf3e0f22de0b589080a772
Stefano-Labianca <lstefano@protonmail.com>
2023-05-08 11:39:24 +0200

    Aggiunta condizione sui flag --help e -h

--- src/main/java/it/uniba/app/App.java
+++ src/main/java/it/uniba/app/App.java
@@ -3,9 +3,11 @@ package it.uniba.app;
 import java.nio.charset.StandardCharsets;
 import java.util.Scanner;
 
+import it.uniba.nygaard.game.Util;
+
 /**
  * <p>
- *   La classe App si occupa della gestione dei comandi da tastiera,
+ * La classe App si occupa della gestione dei comandi da tastiera,
  * vede se ci sono flags in ingresso all'avvio dell'applicazione
  * e in tal caso esegue le funzioni associate.
  * </p>
@@ -23,6 +25,7 @@ public final class App {
   public String getGreeting() {
     return "Hello World!!!";
   }
+
   /**
    * Punto di ingresso dell'applicazione.
    *
@@ -32,25 +35,30 @@ public final class App {
     Scanner in = new Scanner(System.in, StandardCharsets.UTF_16);
     String command;
     // Creazione del match
-    /*
-    if (args.length > 0 && (args[0].equals("--help") || args[0].equals("-h"))) {
-        Stampa help
+
+    String argument = args[0].toLowerCase();
+
+    if (args.length > 0) {
+      if (argument.equals("--help") || argument.equals("-h")) {
+        Util.printHelp();
+      }
     }
-    */
+
     while (true) {
       System.out.print("Inserire un comando: ");
       command = in.nextLine().toLowerCase();
-      /* case "/help" -> Stampa help
-                case "/esci" -> Esegui comando di uscita
-                case "/facile" -> Setta la difficolta a facile
-                case "/medio" -> Setta la difficolta a medio
-                case "/difficile" -> Setta la difficolta a difficile
-                case "/mostralivello" -> Mostrare il livello
-                case "/mostranavi" -> Mostrare le navi e
-                il numero di occorrenze di esse
-                case "/gioca" -> Incomincia la partita
-                case "/svelagriglia" -> Svela la griglia con tanto di navi
-                 */
+      /*
+       * case "/help" -> Stampa help
+       * case "/esci" -> Esegui comando di uscita
+       * case "/facile" -> Setta la difficolta a facile
+       * case "/medio" -> Setta la difficolta a medio
+       * case "/difficile" -> Setta la difficolta a difficile
+       * case "/mostralivello" -> Mostrare il livello
+       * case "/mostranavi" -> Mostrare le navi e
+       * il numero di occorrenze di esse
+       * case "/gioca" -> Incomincia la partita
+       * case "/svelagriglia" -> Svela la griglia con tanto di navi
+       */
       System.out.println("Comando non riconosciuto");
     }
   }
