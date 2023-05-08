c4948abc4e3a71e68462f9df3ee062df9bd32139
Stefano-Labianca <lstefano@protonmail.com>
2023-05-08 17:05:29 +0200

    Completato metodo printHelp()

--- src/main/java/it/uniba/nygaard/game/Util.java
+++ src/main/java/it/uniba/nygaard/game/Util.java
@@ -1,15 +1,35 @@
 package it.uniba.nygaard.game;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 /**
  * <p>
  * La classe Util contiene tutte le costanti globali
  * e funzioni di servizio.
  * </p>
  */
-public class Util {
+public final class Util {
 
+    private Util() { }
+
+    /**
+     * <p>
+     * Mostra una descrizione del programma insieme a tutti
+     * i comandi disponibili che si possono eseguire.
+     * </p>
+     */
     public static void printHelp() {
+        Path filePath = Paths.get("src/main/java/it/uniba/nygaard/game/help/help.txt");
 
+        try {
+            String result = Files.readString(filePath);
+            System.out.println(result);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
 }
