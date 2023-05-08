b3d5c51450203d4ea085dda47d61f1773f30216c
Stefano-Labianca <lstefano@protonmail.com>
2023-05-08 11:35:21 +0200

    Aggiunto metodo di stampa delle comando /help

--- src/main/java/it/uniba/nygaard/game/Util.java
+++ src/main/java/it/uniba/nygaard/game/Util.java
@@ -2,10 +2,14 @@ package it.uniba.nygaard.game;
 
 /**
  * <p>
- *   La classe Util contiene tutte le costanti globali
- *   e funzioni di servizio.
+ * La classe Util contiene tutte le costanti globali
+ * e funzioni di servizio.
  * </p>
  */
 public class Util {
+
+    public static void printHelp() {
+
+    }
 
 }
