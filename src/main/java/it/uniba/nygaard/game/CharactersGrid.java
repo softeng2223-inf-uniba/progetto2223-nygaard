d3d21b4338065198950ece79f46f9a51109aaaa9
Davide Cirilli <107247984+Davy592@users.noreply.github.com>
2023-05-10 23:16:39 +0200

    Aggiunto metodo toString alla classe CharactersGrid

--- src/main/java/it/uniba/nygaard/game/CharactersGrid.java
+++ src/main/java/it/uniba/nygaard/game/CharactersGrid.java
@@ -22,4 +22,42 @@ public class CharactersGrid extends Grid {
     return grid[row][column];
   }
 
+  String toString(Grid grid) {
+    String str = "";
+    char c;
+    str += Util.concatCharacters('-', Util.HEADING_EDGE_WIDTH);
+    str += "Attacco";
+    str += Util.concatCharacters('-', Util.HEADING_EDGE_WIDTH);
+    str += "\n\n";
+    str += Util.concatCharacters(' ', Util.COLUMNS_EDGE_WIDTH);
+    for (char y = Util.MIN_COLUMN; y <= Util.MAX_COLUMN; y++) {
+      str += y + " ";
+    }
+    str += "\n\n";
+    for (int x = Util.MIN_ROWS; x <= Util.MAX_ROWS; x++) {
+      str += Util.concatCharacters(' ', Util.ROWS_EDGE_WIDTH);
+      if (x < 10) {
+        str += " ";
+      }
+      str += x + "   ";
+      for (char y = Util.MIN_COLUMN; y <= Util.MAX_COLUMN; y++) {
+        c = ((CharactersGrid) grid).getCharacter(x - 1, y - 'A');
+        str += c + " ";
+      }
+      str += "  " + x + "\n";
+    }
+    str += "\n";
+    str += Util.concatCharacters(' ', Util.COLUMNS_EDGE_WIDTH);
+    for (char y = Util.MIN_COLUMN; y <= Util.MAX_COLUMN; y++) {
+      str += y + " ";
+    }
+    str += Util.concatCharacters(' ', Util.COLUMNS_EDGE_WIDTH - 1);
+    str += "\n\n";
+    str += Util.concatCharacters('-', Util.HEADING_EDGE_WIDTH);
+    str += "Attacco";
+    str += Util.concatCharacters('-', Util.HEADING_EDGE_WIDTH);
+    str += "\n";
+    return str;
+  }
+
 }
