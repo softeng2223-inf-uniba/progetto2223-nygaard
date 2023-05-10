6a764bc6ced5795c108436348fe68df9780412e6
Davide Cirilli <107247984+Davy592@users.noreply.github.com>
2023-05-10 22:41:14 +0200

    Aggiunto costruttore classe CharactersGrid

--- src/main/java/it/uniba/nygaard/game/CharactersGrid.java
+++ src/main/java/it/uniba/nygaard/game/CharactersGrid.java
@@ -4,4 +4,14 @@ public class CharactersGrid extends Grid {
 
   char[][] grid;
 
+  public CharactersGrid(int dimension) {
+    super(dimension);
+    grid = new char[dimension][dimension];
+    for (int i = 0; i < dimension; i++) {
+      for (int j = 0; j < dimension; j++) {
+        grid[i][j] = '~';
+      }
+    }
+  }
+
 }
