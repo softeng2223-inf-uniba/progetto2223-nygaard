4b8833e90024586093bf926a0acc5cb0b1cd2d3f
Davide Cirilli <107247984+Davy592@users.noreply.github.com>
2023-05-10 23:03:58 +0200

    Aggiunto costruttore alla classe CellsGrid

--- src/main/java/it/uniba/nygaard/game/CellsGrid.java
+++ src/main/java/it/uniba/nygaard/game/CellsGrid.java
@@ -4,4 +4,14 @@ public class CellsGrid extends Grid {
 
   Cell[][] grid;
 
+  public CellsGrid(int dimension) {
+    super(dimension);
+    grid = new Cell[dimension][dimension];
+    for (int i = 0; i < dimension; i++) {
+      for (int j = 0; j < dimension; j++) {
+        grid[i][j] = new Cell('~');
+      }
+    }
+  }
+
 }
