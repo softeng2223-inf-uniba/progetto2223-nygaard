4c1d36c8c6ec10d9519fc543dd408287a859a5a3
Davide Cirilli <107247984+Davy592@users.noreply.github.com>
2023-05-10 22:36:02 +0200

    Predisposto metodo `/esci`

--- src/main/java/it/uniba/app/App.java
+++ src/main/java/it/uniba/app/App.java
@@ -56,6 +56,7 @@ public final class App {
 
       switch (command) {
         case "/help" -> Util.printHelp();
+        //case "/esci" -> l'app chiede conferma, in caso affermativo l'app si chiude
         case "/facile" -> p.setDifficulty(Util.DIFFICULTY_EASY);
         case "/medio" -> p.setDifficulty(Util.DIFFICULTY_MEDIUM);
         case "/difficile" -> p.setDifficulty(Util.DIFFICULTY_HARD);
