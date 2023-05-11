e5bf5304c08c2c6a4696b5bcca0ba2d4652326d3
Davide Cirilli <107247984+Davy592@users.noreply.github.com>
2023-05-11 22:37:53 +0200

    Aggiunti JavaDoc classe AircraftCarrier

--- src/main/java/it/uniba/nygaard/game/ships/AircraftCarrier.java
+++ src/main/java/it/uniba/nygaard/game/ships/AircraftCarrier.java
@@ -2,8 +2,18 @@ package it.uniba.nygaard.game.ships;
 
 import it.uniba.nygaard.game.Util;
 
+/**
+ * <p>
+ * La classe AircraftCarrier rappresenta la nave Portaerei.
+ * </p>
+ */
 public class AircraftCarrier extends Ship {
 
+  /**
+   * <p>
+   * Costruttore della classe AircraftCarrier.
+   * </p>
+   */
   public AircraftCarrier() {
     super(Util.AIRCRAFT_CARRIER_HP);
   }
