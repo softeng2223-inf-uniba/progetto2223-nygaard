37f4e59b54c368003a4a998ea9bcbb2e3837be20
Davide Cirilli <107247984+Davy592@users.noreply.github.com>
2023-05-11 23:41:31 +0200

    Aggiunti JavaDoc classe Grid

--- src/main/java/it/uniba/nygaard/game/grids/Grid.java
+++ src/main/java/it/uniba/nygaard/game/grids/Grid.java
@@ -1,9 +1,22 @@
 package it.uniba.nygaard.game.grids;
 
+/**
+ * <h2> Grid </h2>
+ * <p>
+ * La classe Grid rappresenta una griglia di gioco.
+ * </p>
+ */
 public abstract class Grid {
 
   private int dimension;
 
+  /**
+   * <h3> Costruttore </h3>
+   * <p>
+   * Costruttore della classe Grid.
+   * </p>
+   * @param newDimension Dimensione della griglia.
+   */
   Grid(final int newDimension) {
     this.dimension = newDimension;
   }
