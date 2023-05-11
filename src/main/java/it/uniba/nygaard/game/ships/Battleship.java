fd1ac5a706fd817e722818001750ff64ea4a44af
Davide Cirilli <107247984+Davy592@users.noreply.github.com>
2023-05-11 22:40:06 +0200

    Aggiunti JavaDoc classe Battleship

--- src/main/java/it/uniba/nygaard/game/ships/Battleship.java
+++ src/main/java/it/uniba/nygaard/game/ships/Battleship.java
@@ -2,8 +2,18 @@ package it.uniba.nygaard.game.ships;
 
 import it.uniba.nygaard.game.Util;
 
+/**
+ * <p>
+ * La classe Battleship rappresenta la Corazzata.
+ * </p>
+ */
 public class Battleship extends Ship {
 
+  /**
+   * <p>
+   * Costruttore della classe Battleship.
+   * </p>
+   */
   public Battleship() {
     super(Util.BATTLESHIP_HP);
   }
