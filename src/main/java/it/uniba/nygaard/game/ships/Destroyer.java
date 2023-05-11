8525a5ae65029f0b603325a552df641a78978f9b
Davide Cirilli <107247984+Davy592@users.noreply.github.com>
2023-05-11 22:58:31 +0200

    Aggiunti JavaDoc classe Destroyer

--- src/main/java/it/uniba/nygaard/game/ships/Destroyer.java
+++ src/main/java/it/uniba/nygaard/game/ships/Destroyer.java
@@ -2,8 +2,20 @@ package it.uniba.nygaard.game.ships;
 
 import it.uniba.nygaard.game.Util;
 
+/**
+ * <h2> Destroyer </h2>
+ * <p>
+ * La classe Destroyer rappresenta la nave Cacciatorpediniere.
+ * @see Ship
+ * </p>
+ */
 public class Destroyer extends Ship {
 
+  /**
+   * <p>
+   * Costruttore della classe Destroyer.
+   * </p>
+   */
   public Destroyer() {
     super(Util.DESTROYER_HP);
   }
