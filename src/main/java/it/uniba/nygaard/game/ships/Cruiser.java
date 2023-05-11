984091f005e995f0c071992c8e29ad62aa27a68d
Davide Cirilli <107247984+Davy592@users.noreply.github.com>
2023-05-11 22:52:34 +0200

    Aggiunti JavaDoc classe Cruiser

--- src/main/java/it/uniba/nygaard/game/ships/Cruiser.java
+++ src/main/java/it/uniba/nygaard/game/ships/Cruiser.java
@@ -2,8 +2,20 @@ package it.uniba.nygaard.game.ships;
 
 import it.uniba.nygaard.game.Util;
 
+/**
+ * <h2> Cruiser </h2>
+ * <p>
+ * La classe Cruiser rappresenta la nave incrociatore.
+ * @see Ship
+ * </p>
+ */
 public class Cruiser extends Ship {
 
+  /**
+   * <p>
+   * Costruttore della classe Cruiser.
+   * </p>
+   */
   public Cruiser() {
     super(Util.CRUISER_HP);
   }
