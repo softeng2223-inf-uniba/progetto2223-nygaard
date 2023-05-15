1d9f52a845219f6886078d9153e17a798ba2d512
Emanuele Fontana <fontanaemanuele14@gmail.com>
2023-05-15 18:08:43 +0200

    Aggiunto colore viola Ho aggiunto la costante per identificare il colore viola nella stampa a video

--- src/main/java/it/uniba/nygaard/game/Util.java
+++ src/main/java/it/uniba/nygaard/game/Util.java
@@ -49,6 +49,8 @@ public final class Util {
   public static final String RED = "\u001B[31m";
   public static final String GREEN = "\u001B[32m";
   public static final String YELLOW = "\u001B[33m";
+  public static final String BLUE = "\u001B[34m";
+  public static final String PURPLE = "\u001B[35m";
 
   /**
    * <h3> Costruttore </h3>
