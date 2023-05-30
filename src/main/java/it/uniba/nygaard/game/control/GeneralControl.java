22bf797ba7123206c193a2900b29034295b52b05
Emanuele Fontana <fontanaemanuele14@gmail.com>
2023-05-30 16:48:52 +0200

    Aggiunta classe GeneralControl

---
+++ src/main/java/it/uniba/nygaard/game/control/GeneralControl.java
@@ -0,0 +1,54 @@
+package it.uniba.nygaard.game.control;
+
+import it.uniba.nygaard.game.Util;
+import it.uniba.nygaard.game.boundary.InputBoundary;
+import it.uniba.nygaard.game.entity.Match;
+import java.util.HashMap;
+
+/**
+ * << Control >>
+ * <h2> GeneralControl </h2>
+ * <p>
+ * La classe GeneralControl rappresenta il controllo generale del gioco.
+ * </p>
+ */
+public final class GeneralControl {
+    private GeneralControl() {
+    }
+    /**
+     * <h3> startGame </h3>
+     * <p>
+     *   Avvia il gioco.
+     * </p>
+     * @param args Argomenti passati al programma.
+     */
+  public static void startGame(final String[] args) {
+    GameManager.setMatch(Match.getInstance());
+    GameManager.setNextDifficulty(Util.DIFFICULTY_NOT_SETTED);
+    GameManager.setArgs(args);
+    HashMap<String, CommandInterface> availableCommands = new HashMap<>();
+    availableCommands.put("/esci", ExitCommand.getInstance());
+    availableCommands.put("/facile", SetEasyDifficulty.getInstance());
+    availableCommands.put("/medio", SetMediumDifficulty.getInstance());
+    availableCommands.put("/difficile", SetHardDifficulty.getInstance());
+    availableCommands.put("/mostralivello", ShowLevelCommand.getInstance());
+    availableCommands.put("/mostranavi", ShowShipCommand.getInstance());
+    availableCommands.put("/gioca", StartMatchCommand.getInstance());
+    availableCommands.put("/svelagriglia", UnveilGridCommand.getInstance());
+    availableCommands.put("/help", HelpCommand.getInstance());
+    ParamControl.getInstance().executeCommand();
+    while (true) {
+          String[] command = InputBoundary.getCommand().split(" ");
+        InputBoundary.reset();
+        if (availableCommands.containsKey(command[0])) {
+          if (command.length == 1) {
+            availableCommands.get(command[0]).executeCommand(null);
+          } else {
+            availableCommands.get(command[0]).executeCommand(null);
+          }
+        } else {
+          InputBoundary.notRecognisedCommand(command[0]);
+        }
+      }
+    }
+  }
