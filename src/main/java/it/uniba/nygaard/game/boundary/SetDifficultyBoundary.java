1aae7f34391415c80b2545a7f660c73f633aafe4
Emanuele Fontana <fontanaemanuele14@gmail.com>
2023-05-30 15:46:25 +0200

    Aggiunto metodo AlreadyInGame

--- src/main/java/it/uniba/nygaard/game/boundary/SetDifficultyBoundary.java
+++ src/main/java/it/uniba/nygaard/game/boundary/SetDifficultyBoundary.java
@@ -1,6 +1,11 @@
 package it.uniba.nygaard.game.boundary;
 
+import it.uniba.nygaard.game.Util;
+
 public class SetDifficultyBoundary {
     private SetDifficultyBoundary() {
+    }
+    public static void alreadyInGame() {
+        System.out.println(Util.RED + "Non puoi cambiare difficoltà durante una partita" + Util.RESET);
     }
 }
