c1c08dbde9557dbdf5b3762fbc1852522d78a6f2
Emanuele Fontana <fontanaemanuele14@gmail.com>
2023-05-30 14:17:19 +0200

    Aggiunto metodo getDescription()

--- src/main/java/it/uniba/nygaard/game/boundary/DescriptionBoundary.java
+++ src/main/java/it/uniba/nygaard/game/boundary/DescriptionBoundary.java
@@ -1,5 +1,21 @@
 package it.uniba.nygaard.game.boundary;
 
+import it.uniba.nygaard.game.Util;
+
 public class DescriptionBoundary {
-    
+    public static void getDescription() {
+        String description = Util.BOLD + Util.PURPLE
+                + "Benvenuti nel gioco della battaglia navale!\n"
+                + Util.RESET
+                + "Prima di iniziare vi andiamo a presentare una breve introduzione al gioco con\n"
+                + "i suoi relativi comandi.\n\n"
+                + Util.YELLOW + Util.BOLD + "> INTRODUZIONE\n" + Util.RESET
+                + "    In questa realizzazione della battaglia navale, giocherete voi contro il vostro\n"
+                + "    computer che posizionera' le navi in automatico prima dell'inizio della partita.\n\n"
+                + "    Come giocatore, dovete individuare le posizioni delle navi e affondarle\n"
+                + "    tutte, dalla prima all'ultima.\n\n"
+                + "    Vincerete la partita solamente se affonderete tutte le navi prima di raggiungere\n"
+                + "    il numero massimo di tentativi falliti, che cambia in base alla difficolta' scelta.\n";
+        System.out.println(description);
+    }
 }
