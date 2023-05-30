883fd3b4b831b1f228782841e863eda22a61bf6c
Emanuele Fontana <fontanaemanuele14@gmail.com>
2023-05-30 16:02:06 +0200

    Aggiunta JavaDoc per ShowShipBoundary

--- src/main/java/it/uniba/nygaard/game/boundary/ShowShipBoundary.java
+++ src/main/java/it/uniba/nygaard/game/boundary/ShowShipBoundary.java
@@ -2,10 +2,28 @@ package it.uniba.nygaard.game.boundary;
 
 import it.uniba.nygaard.game.Util;
 
+/**
+ * <<Boundary>>
+ *<h2> ShowShipBoundary </h2>
+ *<p>
+ *La classe ShowShipBoundary stampa le navi disponibili.
+ *</p>
+ */
 public class ShowShipBoundary {
+    /**
+     * <h3> Costruttore </h3>
+     * <p>
+     * Costruttore della classe ShowShipBoundary.
+     * </p>
+     */
     private ShowShipBoundary() {
     }
-
+    /**
+     * <h3> showShips </h3>
+     * <p>
+     * Stampa le navi disponibili.
+     * </p>
+     */
     public static void showShips() {
         System.out.println(Util.CYAN + Util.ITALIC + "\tCacciatorpediniere \t" + Util.RESET + "■■ \t"
                 + Util.CYAN + Util.BOLD + "Esemplari: " + Util.DESTROYER_NO + Util.RESET);
