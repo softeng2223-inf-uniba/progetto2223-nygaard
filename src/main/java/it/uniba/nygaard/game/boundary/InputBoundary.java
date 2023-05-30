18f41bb93e7e36fb4726c90493b96bbf291017bc
Emanuele Fontana <fontanaemanuele14@gmail.com>
2023-05-30 15:01:18 +0200

    Aggiunta JavaDoc per InputBoundary

--- src/main/java/it/uniba/nygaard/game/boundary/InputBoundary.java
+++ src/main/java/it/uniba/nygaard/game/boundary/InputBoundary.java
@@ -5,20 +5,53 @@ import it.uniba.nygaard.game.Util;
 import java.nio.charset.StandardCharsets;
 import java.util.Scanner;
 
+/**
+ * << Boundary >>
+ * <h2> InputBoundary </h2>
+ * <p>
+ * La classe InputBoundary rappresenta la classe che si occupa di gestire
+ * l'input da tastiera.
+ * </p>
+ */
 public class InputBoundary {
+    /**
+     * <h3> Costruttore </h3>
+     * <p>
+     * Costruttore della classe InputBoundary.
+     * </p>
+     */
     private InputBoundary() {
     }
 
+    /**
+     * <h3> getCommand </h3>
+     * <p>
+     *     Metodo che chiede all'utente di inserire un comando.
+     *     Il comando inserito viene convertito in minuscolo e restituito.
+     * </p>
+     * @return Stringa che rappresenta il comando inserito dall'utente.
+     */
     public static String getCommand() {
         System.out.print("Inserire un comando: " + Util.CYAN);
         return new Scanner(System.in, StandardCharsets.UTF_8).nextLine().toLowerCase();
     }
-    public static void notRecognisedCommand(String command)
-    {
+    /**
+     * <h3> notRecognisedCommand </h3>
+     * <p>
+     *     Metodo che stampa a video il messaggio di comando non riconosciuto.
+     * </p>
+     * @param command Stringa che rappresenta il comando non riconosciuto.
+     */
+    public static void notRecognisedCommand(String command) {
         System.out.println(Util.RED + "Comando non riconosciuto: " + command + Util.RESET);
     }
-    public static void reset()
-    {
+    /**
+     * <h3> reset </h3>
+     * <p>
+     *     Metodo che resetta il colore del testo a quello di default.
+     * </p>
+     */
+    public static void reset() {
         System.out.print(Util.RESET);
     }
 }
