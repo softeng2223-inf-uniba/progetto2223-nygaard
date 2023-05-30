3599c091904613afc7e4aff9796377f07192e38e
Stefano-Labianca <lstefano@protonmail.com>
2023-05-31 00:02:45 +0200

    Pulito del codice

--- src/main/java/it/uniba/nygaard/game/boundary/HelpBoundary.java
+++ src/main/java/it/uniba/nygaard/game/boundary/HelpBoundary.java
@@ -22,39 +22,40 @@ public final class HelpBoundary {
      */
     public static void getHelp() {
         DescriptionBoundary.getDescription();
-        String help = Util.YELLOW
-                + Util.BOLD + "> COMANDI\n" + Util.RESET
-                + Util.ITALIC + Util.CYAN + "    * /help             " + Util.RESET
-                + "Vengono visualizzate le regole " + "di gioco e i comandi disponibili\n"
-                + Util.ITALIC + Util.CYAN + "    * /esci             " + Util.RESET
-                + "Ti permette di uscire dal gioco\n"
-                + Util.ITALIC + Util.CYAN + "    * /facile           " + Util.RESET
-                + "Imposta la difficolta' della partita a \"Facile\"\n"
-                + Util.ITALIC + Util.CYAN + "    * /medio            " + Util.RESET
-                + "Imposta la difficolta' della partita a \"Medio\"\n"
-                + Util.ITALIC + Util.CYAN + "    * /difficile        " + Util.RESET
-                + "Imposta la difficolta' della partita a \"Difficile\"\n"
-                + Util.ITALIC + Util.CYAN + "    * /mostralivello    " + Util.RESET
-                + "Mostra il livello della partita\n"
-                + Util.ITALIC + Util.CYAN + "    * /mostranavi       " + Util.RESET
-                + "Mostra le navi ancora in gioco e il loro numero\n"
-                + Util.ITALIC + Util.CYAN + "    * /gioca            " + Util.RESET
-                + "Ti fa iniziare una partita\n"
-                + Util.ITALIC + Util.CYAN + "    * /svelagriglia     " + Util.RESET
-                + "Mostra la griglia generata dal computer\n\n"
-                + HelpBoundary.subCommandsDifficulty();
+        String help = mainCommands() + subCommandsDifficulty();
         System.out.println(help);
     }
 
 
     private static String mainCommands() {
-        return "";
+        String mainCommands = Util.YELLOW
+            + Util.BOLD + "> COMANDI\n" + Util.RESET
+            + Util.ITALIC + Util.CYAN + "    * /help             " + Util.RESET
+            + "Vengono visualizzate le regole " + "di gioco e i comandi disponibili\n"
+            + Util.ITALIC + Util.CYAN + "    * /esci             " + Util.RESET
+            + "Ti permette di uscire dal gioco\n"
+            + Util.ITALIC + Util.CYAN + "    * /facile           " + Util.RESET
+            + "Imposta la difficolta' della partita a \"Facile\"\n"
+            + Util.ITALIC + Util.CYAN + "    * /medio            " + Util.RESET
+            + "Imposta la difficolta' della partita a \"Medio\"\n"
+            + Util.ITALIC + Util.CYAN + "    * /difficile        " + Util.RESET
+            + "Imposta la difficolta' della partita a \"Difficile\"\n"
+            + Util.ITALIC + Util.CYAN + "    * /mostralivello    " + Util.RESET
+            + "Mostra il livello della partita\n"
+            + Util.ITALIC + Util.CYAN + "    * /mostranavi       " + Util.RESET
+            + "Mostra le navi ancora in gioco e il loro numero\n"
+            + Util.ITALIC + Util.CYAN + "    * /gioca            " + Util.RESET
+            + "Ti fa iniziare una partita\n"
+            + Util.ITALIC + Util.CYAN + "    * /svelagriglia     " + Util.RESET
+            + "Mostra la griglia generata dal computer\n\n";
+
+        return mainCommands;
     }
 
 
     private static String subCommandsDifficulty() {
         String subDifficulty = Util.YELLOW
-            + Util.BOLD + "> DIFFICOLTA' CUSTOM\n" + Util.RESET
+            + Util.BOLD + "> DIFFICOLTA' PERSONALIZZATE\n" + Util.RESET
             + Util.ITALIC + Util.CYAN + "    * /facile numero    " + Util.RESET
             + "Imposta il numero massimo di tentativi, per la difficolta' facile, ad un valore pari a"
             + Util.ITALIC + " numero " + Util.RESET;
