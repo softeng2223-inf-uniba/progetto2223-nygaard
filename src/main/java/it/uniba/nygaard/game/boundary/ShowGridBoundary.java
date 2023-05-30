a9282f463e5e3a4a6d9bde2fcdcbd022b694ecf6
Emanuele Fontana <fontanaemanuele14@gmail.com>
2023-05-30 15:53:02 +0200

    Aggiunto metodo notInGame

--- src/main/java/it/uniba/nygaard/game/boundary/ShowGridBoundary.java
+++ src/main/java/it/uniba/nygaard/game/boundary/ShowGridBoundary.java
@@ -1,6 +1,12 @@
 package it.uniba.nygaard.game.boundary;
 
+import it.uniba.nygaard.game.Util;
+
 public class ShowGridBoundary {
     private ShowGridBoundary() {
+    }
+    public static void notInGame()
+    {
+        System.out.println(Util.RED + "Non sei in partita" + Util.RESET);
     }
 }
