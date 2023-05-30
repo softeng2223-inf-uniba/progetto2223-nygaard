025b705a240f8626a00c4b6117a835ab07e768c7
Emanuele Fontana <fontanaemanuele14@gmail.com>
2023-05-30 15:44:46 +0200

    Aggiunta JavaDoc di ParamsBoundary

--- src/main/java/it/uniba/nygaard/game/boundary/ParamsBoundary.java
+++ src/main/java/it/uniba/nygaard/game/boundary/ParamsBoundary.java
@@ -5,18 +5,46 @@ import it.uniba.nygaard.game.Util;
 import java.nio.charset.StandardCharsets;
 import java.util.Scanner;
 
+/**
+ * <<Boundary>>
+ * <h2> ParamsBoundary </h2>
+ * <p>
+ * La classe ParamsBoundary stampa i messaggi di errore
+ * relativi ai parametri passati in ingresso all'avvio
+ * del gioco.
+ * </p>
+ */
 public class ParamsBoundary {
-    private ParamsBoundary(){
+    /**
+     * <h3> Costruttore </h3>
+     * <p>
+     * Costruttore della classe ParamsBoundary.
+     * </p>
+     */
+    private ParamsBoundary() {
     }
 
-    public static void tooManyArgs()
-    {
+    /**
+     * <h3> tooManyArgs </h3>
+     * <p>
+     * Stampa il messaggio di errore relativo al numero
+     * di parametri passati in ingresso all'avvio del gioco.
+     * </p>
+     */
+    public static void tooManyArgs() {
         System.out.println(Util.RED + "Troppi parametri inseriti.\nPremere invio per uscire" + Util.RESET);
         new Scanner(System.in, StandardCharsets.UTF_8).nextLine();
     }
 
-    public static void notRecogniseArg(String argument)
-    {
+    /**
+     * <h3> notRecogniseArg </h3>
+     * <p>
+     * Stampa il messaggio di errore relativo al parametro non valido
+     * passato in ingresso all'avvio del gioco.
+     * </p>
+     * @param argument Parametro non riconosciuto
+     */
+    public static void notRecogniseArg(String argument) {
         System.out.println(Util.RED + "Parametro " + argument + " non riconosciuto.\nPremere invio per uscire"
                 + Util.RESET);
         new Scanner(System.in, StandardCharsets.UTF_8).nextLine();
