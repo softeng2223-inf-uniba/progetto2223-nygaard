df442ae3720f1cf5645dad93fa6f5ba5d1be227f
Emanuele Fontana <fontanaemanuele14@gmail.com>
2023-05-30 14:51:46 +0200

    Aggiunta javadoc per HelpBoundary

--- src/main/java/it/uniba/nygaard/game/boundary/HelpBoundary.java
+++ src/main/java/it/uniba/nygaard/game/boundary/HelpBoundary.java
@@ -2,10 +2,24 @@ package it.uniba.nygaard.game.boundary;
 
 import it.uniba.nygaard.game.Util;
 
+/**
+ * << Boundary >>
+ * <h2> HelpBoundary </h2>
+ * <p>
+ * La classe HelpBoundary rappresenta la schermata di aiuto.
+ * </p>
+ *
+ * @see DescriptionBoundary
+ */
 public class HelpBoundary {
     private HelpBoundary() {
     }
-
+    /**
+     * <h3> getHelp </h3>
+     * <p>
+     * Metodo che stampa a schermo la schermata di aiuto.
+     * </p>
+     */
     public static void getHelp() {
         DescriptionBoundary.getDescription();
         String help = Util.YELLOW
