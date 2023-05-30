923940d52d173be45b261e3779e1ce26b513bca7
Emanuele Fontana <fontanaemanuele14@gmail.com>
2023-05-30 15:57:13 +0200

    Aggiunta JavaDoc per ShowLevelBoundary

--- src/main/java/it/uniba/nygaard/game/boundary/ShowLevelBoundary.java
+++ src/main/java/it/uniba/nygaard/game/boundary/ShowLevelBoundary.java
@@ -1,8 +1,29 @@
 package it.uniba.nygaard.game.boundary;
 
+/**
+ * <<Boundary>>
+ * <h2> ShowLevelBoundary </h2>
+ * <p>
+ * La classe ShowLevelBoundary stampa il livello di difficoltà
+ * scelto dall'utente.
+ * </p>
+ */
 public class ShowLevelBoundary {
+    /**
+     * <h3> Costruttore </h3>
+     * <p>
+     * Costruttore della classe ShowLevelBoundary.
+     * </p>
+     */
     private ShowLevelBoundary() {
     }
+    /**
+     * <h3> printActualDifficulty </h3>
+     * <p>
+     * Stampa il livello di difficoltà scelto dall'utente.
+     * </p>
+     * @param s Livello di difficoltà
+     */
     public static void printActualDifficulty(String s) {
         System.out.println(s);
     }
