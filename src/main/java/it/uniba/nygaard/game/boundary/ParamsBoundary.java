dd3e64189599fd16079eb98f63da467e4ddd26df
Emanuele Fontana <fontanaemanuele14@gmail.com>
2023-05-30 15:42:47 +0200

    Aggiunto metodo tooManyArgs

--- src/main/java/it/uniba/nygaard/game/boundary/ParamsBoundary.java
+++ src/main/java/it/uniba/nygaard/game/boundary/ParamsBoundary.java
@@ -1,6 +1,17 @@
 package it.uniba.nygaard.game.boundary;
 
+import it.uniba.nygaard.game.Util;
+
+import java.nio.charset.StandardCharsets;
+import java.util.Scanner;
+
 public class ParamsBoundary {
     private ParamsBoundary(){
+    }
+
+    public static void tooManyArgs()
+    {
+        System.out.println(Util.RED + "Troppi parametri inseriti.\nPremere invio per uscire" + Util.RESET);
+        new Scanner(System.in, StandardCharsets.UTF_8).nextLine();
     }
 }
