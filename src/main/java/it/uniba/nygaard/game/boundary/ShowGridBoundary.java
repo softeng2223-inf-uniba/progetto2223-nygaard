00ff3cb21a8e5cc541d904196de1bd8aa463b468
Emanuele Fontana <fontanaemanuele14@gmail.com>
2023-05-30 15:53:19 +0200

    Aggiunto metodo printGrid

--- src/main/java/it/uniba/nygaard/game/boundary/ShowGridBoundary.java
+++ src/main/java/it/uniba/nygaard/game/boundary/ShowGridBoundary.java
@@ -1,6 +1,7 @@
 package it.uniba.nygaard.game.boundary;
 
 import it.uniba.nygaard.game.Util;
+import it.uniba.nygaard.game.entity.grids.Grid;
 
 public class ShowGridBoundary {
     private ShowGridBoundary() {
@@ -8,5 +9,9 @@ public class ShowGridBoundary {
     public static void notInGame()
     {
         System.out.println(Util.RED + "Non sei in partita" + Util.RESET);
+    }
+    public static void printGrid(Grid grid)
+    {
+        System.out.println(grid);
     }
 }
