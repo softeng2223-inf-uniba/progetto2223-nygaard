04d6f42872cf90dc18d9c3fb22d7ab29603a483c
Emanuele Fontana <fontanaemanuele14@gmail.com>
2023-05-30 15:54:10 +0200

    Aggiunta JavaDoc di ShowGridBoundary

--- src/main/java/it/uniba/nygaard/game/boundary/ShowGridBoundary.java
+++ src/main/java/it/uniba/nygaard/game/boundary/ShowGridBoundary.java
@@ -3,15 +3,40 @@ package it.uniba.nygaard.game.boundary;
 import it.uniba.nygaard.game.Util;
 import it.uniba.nygaard.game.entity.grids.Grid;
 
+/**
+ * <<Boundary>>
+ * <h2> ShowGridBoundary </h2>
+ * <p>
+ * La classe ShowGridBoundary stampa la griglia di gioco.
+ * </p>
+ */
 public class ShowGridBoundary {
+    /**
+     * <h3> Costruttore </h3>
+     * <p>
+     * Costruttore della classe ShowGridBoundary.
+     * </p>
+     */
     private ShowGridBoundary() {
     }
-    public static void notInGame()
-    {
+    /**
+     * <h3> notInGame </h3>
+     * <p>
+     * Stampa il messaggio di errore relativo al tentativo di
+     * stampare la griglia di gioco senza essere in partita.
+     * </p>
+     */
+    public static void notInGame() {
         System.out.println(Util.RED + "Non sei in partita" + Util.RESET);
     }
-    public static void printGrid(Grid grid)
-    {
+    /**
+     * <h3> printGrid </h3>
+     * <p>
+     * Stampa la griglia di gioco.
+     * </p>
+     * @param grid Griglia di gioco
+     */
+    public static void printGrid(Grid grid) {
         System.out.println(grid);
     }
 }
