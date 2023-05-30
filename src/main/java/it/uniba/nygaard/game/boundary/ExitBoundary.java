7617b20039b9859c0c7f1db129ea206c72acb9e8
Emanuele Fontana <fontanaemanuele14@gmail.com>
2023-05-30 14:40:22 +0200

    Aggiungo metodo exitString()

--- src/main/java/it/uniba/nygaard/game/boundary/ExitBoundary.java
+++ src/main/java/it/uniba/nygaard/game/boundary/ExitBoundary.java
@@ -1,7 +1,15 @@
 package it.uniba.nygaard.game.boundary;
+import it.uniba.nygaard.game.Util
 
 public class ExitBoundary {
 
     private ExitBoundary(){
     }
+
+    public static void exitString()
+    {
+        System.out.println(Util.YELLOW + Util.BOLD +  "GRAZIE PER AVER GIOCATO!" + Util.RESET);
+    }
+
+
 }
