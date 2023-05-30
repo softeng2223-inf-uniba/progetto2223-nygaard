dc48dc754ea94a238d8c7a86b4cc4dcb20147a29
Emanuele Fontana <fontanaemanuele14@gmail.com>
2023-05-30 16:03:51 +0200

    Aggiunto metodo noDifficulty

--- src/main/java/it/uniba/nygaard/game/boundary/MatchBoundary.java
+++ src/main/java/it/uniba/nygaard/game/boundary/MatchBoundary.java
@@ -1,6 +1,12 @@
 package it.uniba.nygaard.game.boundary;
 
+import it.uniba.nygaard.game.Util;
+
 public class MatchBoundary {
     private MatchBoundary() {
+    }
+    public static void noDifficulty()
+    {
+        System.out.println(Util.RED + "Non puoi iniziare una partita senza impostare il livello di difficoltà" + Util.RESET);
     }
 }
