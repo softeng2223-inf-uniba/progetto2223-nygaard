c00ba969ed389572c33602dc8f3ab7bb2a705022
Emanuele Fontana <fontanaemanuele14@gmail.com>
2023-05-30 14:41:11 +0200

    Aggiunto metodo ask()

--- src/main/java/it/uniba/nygaard/game/boundary/ExitBoundary.java
+++ src/main/java/it/uniba/nygaard/game/boundary/ExitBoundary.java
@@ -1,6 +1,9 @@
 package it.uniba.nygaard.game.boundary;
 import it.uniba.nygaard.game.Util
 
+import java.nio.charset.StandardCharsets;
+import java.util.Scanner;
+
 public class ExitBoundary {
 
     private ExitBoundary(){
@@ -11,5 +14,15 @@ public class ExitBoundary {
         System.out.println(Util.YELLOW + Util.BOLD +  "GRAZIE PER AVER GIOCATO!" + Util.RESET);
     }
 
+    public static String ask()
+    {
+        Scanner in = new Scanner(System.in, StandardCharsets.UTF_8);
+        String command;
+        System.out.print("Vuoi veramente uscire? "
+                + "(" + Util.GREEN + "y" + Util.RESET
+                + "/" + Util.RED + "n" + Util.RESET + ") ");
+        command = in.nextLine().toLowerCase();
+        return command;
+    }
 
 }
