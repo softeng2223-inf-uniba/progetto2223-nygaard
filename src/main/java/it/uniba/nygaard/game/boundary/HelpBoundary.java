bd56980e22854132998b3d5c5ad027372ccf5383
Emanuele Fontana <fontanaemanuele14@gmail.com>
2023-05-30 14:50:31 +0200

    Aggiunto metodo getHelp()

--- src/main/java/it/uniba/nygaard/game/boundary/HelpBoundary.java
+++ src/main/java/it/uniba/nygaard/game/boundary/HelpBoundary.java
@@ -1,6 +1,33 @@
 package it.uniba.nygaard.game.boundary;
 
+import it.uniba.nygaard.game.Util;
+
 public class HelpBoundary {
     private HelpBoundary() {
+    }
+
+    public static void getHelp() {
+        DescriptionBoundary.getDescription();
+        String help = Util.YELLOW
+                + Util.BOLD + "> COMANDI\n" + Util.RESET
+                + Util.ITALIC + Util.CYAN + "    * /help             " + Util.RESET
+                + "Vengono visualizzate le regole " + "di gioco e i comandi disponibili\n"
+                + Util.ITALIC + Util.CYAN + "    * /esci             " + Util.RESET
+                + "Ti permette di uscire dal gioco\n"
+                + Util.ITALIC + Util.CYAN + "    * /facile           " + Util.RESET
+                + "Imposta la difficolta' della partita a \"Facile\"\n"
+                + Util.ITALIC + Util.CYAN + "    * /medio            " + Util.RESET
+                + "Imposta la difficolta' della partita a \"Medio\"\n"
+                + Util.ITALIC + Util.CYAN + "    * /difficile        " + Util.RESET
+                + "Imposta la difficolta' della partita a \"Difficile\"\n"
+                + Util.ITALIC + Util.CYAN + "    * /mostralivello    " + Util.RESET
+                + "Mostra il livello della partita\n"
+                + Util.ITALIC + Util.CYAN + "    * /mostranavi       " + Util.RESET
+                + "Mostra le navi ancora in gioco e il loro numero\n"
+                + Util.ITALIC + Util.CYAN + "    * /gioca            " + Util.RESET
+                + "Ti fa iniziare una partita\n"
+                + Util.ITALIC + Util.CYAN + "    * /svelagriglia     " + Util.RESET
+                + "Mostra la griglia generata dal computer\n";
+        System.out.println(help);
     }
 }
