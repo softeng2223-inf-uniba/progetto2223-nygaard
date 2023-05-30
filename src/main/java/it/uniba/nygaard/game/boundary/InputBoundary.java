f240279c72f0da11440a9be49c93a0776686c80a
Emanuele Fontana <fontanaemanuele14@gmail.com>
2023-05-30 14:56:40 +0200

    Aggiunto metodo getCommand()

--- src/main/java/it/uniba/nygaard/game/boundary/InputBoundary.java
+++ src/main/java/it/uniba/nygaard/game/boundary/InputBoundary.java
@@ -1,6 +1,17 @@
 package it.uniba.nygaard.game.boundary;
 
+import it.uniba.nygaard.game.Util;
+
+import java.nio.charset.StandardCharsets;
+import java.util.Scanner;
+
 public class InputBoundary {
     private InputBoundary() {
+    }
+
+    public static String getCommand() {
+        System.out.print("Inserire un comando: " + Util.CYAN);
+        return new Scanner(System.in, StandardCharsets.UTF_8).nextLine().toLowerCase();
+
     }
 }
