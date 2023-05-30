15c03a5a78ab8684ffe4cfd64e8795f02b4bffea
Emanuele Fontana <fontanaemanuele14@gmail.com>
2023-05-30 15:05:41 +0200

    Aggiunta JavaDoc per LogoBoundary

--- src/main/java/it/uniba/nygaard/game/boundary/LogoBoundary.java
+++ src/main/java/it/uniba/nygaard/game/boundary/LogoBoundary.java
@@ -2,9 +2,28 @@ package it.uniba.nygaard.game.boundary;
 
 import it.uniba.nygaard.game.Util;
 
+/**
+ * <<Boundary>>
+ * <h2> LogoBoundary </h2>
+ * <p>
+ * La classe LogoBoundary stampa il logo del gioco.
+ * </p>
+ */
 public class LogoBoundary {
+    /**
+     * <h3> Costruttore </h3>
+     * <p>
+     * Costruttore della classe LogoBoundary.
+     * </p>
+     */
     private LogoBoundary() {
     }
+    /**
+     * <h3> printLogo </h3>
+     * <p>
+     * Stampa il logo del gioco.
+     * </p>
+     */
     public static void printLogo() {
         String logo = Util.BOLD + Util.RED
                 + "    ____   ___   ______ ______ __     ______ _____  __  __ ____ ____ \n"
