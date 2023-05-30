c50d43e5505dd889dc998f1fb2567490718f7a55
Emanuele Fontana <fontanaemanuele14@gmail.com>
2023-05-30 15:03:58 +0200

    Aggiunto metodo printLogo

--- src/main/java/it/uniba/nygaard/game/boundary/LogoBoundary.java
+++ src/main/java/it/uniba/nygaard/game/boundary/LogoBoundary.java
@@ -1,6 +1,19 @@
 package it.uniba.nygaard.game.boundary;
 
+import it.uniba.nygaard.game.Util;
+
 public class LogoBoundary {
     private LogoBoundary() {
+    }
+    public static void printLogo() {
+        String logo = Util.BOLD + Util.RED
+                + "    ____   ___   ______ ______ __     ______ _____  __  __ ____ ____ \n"
+                + "   / __ ) /   | /_  __//_  __// /    / ____// ___/ / / / //  _// __ \\\n"
+                + "  / __  |/ /| |  / /    / /  / /    / __/   \\__ \\ / /_/ / / / / /_/ /\n"
+                + Util.BLUE
+                + " / /_/ // ___ | / /    / /  / /___ / /___  ___/ // __  /_/ / / ____/ \n"
+                + "/_____//_/  |_|/_/    /_/  /_____//_____/ /____//_/ /_//___//_/      \n"
+                + Util.RESET;
+        System.out.println(logo);
     }
 }
