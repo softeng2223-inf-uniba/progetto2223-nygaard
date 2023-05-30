c9f6787e24d30019ab8541aae94fb6ce104d9e93
Emanuele Fontana <fontanaemanuele14@gmail.com>
2023-05-30 14:48:05 +0200

    Aggiunta javadoc in ExitBoundary

--- src/main/java/it/uniba/nygaard/game/boundary/ExitBoundary.java
+++ src/main/java/it/uniba/nygaard/game/boundary/ExitBoundary.java
@@ -1,21 +1,46 @@
 package it.uniba.nygaard.game.boundary;
-import it.uniba.nygaard.game.Util
+import it.uniba.nygaard.game.Util;
 
 import java.nio.charset.StandardCharsets;
 import java.util.Scanner;
 
+/**
+ * << Boundary >>
+ * <h2> ExitBoundary </h2>
+ * <p>
+ * La classe ExitBoundary rappresenta la classe che si occupa di gestire
+ * l'uscita dal gioco.
+ * </p>
+ */
 public class ExitBoundary {
 
-    private ExitBoundary(){
+    /**
+     * <h3> Costruttore </h3>
+     * <p>
+     * Costruttore della classe ExitBoundary.
+     * </p>
+     */
+    private ExitBoundary() {
     }
-
-    public static void exitString()
-    {
+    /**
+     * <h3> exitString </h3>
+     * <p>
+     * Metodo che stampa a video il messaggio di uscita dal gioco.
+     * </p>
+     */
+    public static void exitString() {
         System.out.println(Util.YELLOW + Util.BOLD +  "GRAZIE PER AVER GIOCATO!" + Util.RESET);
     }
 
-    public static String ask()
-    {
+    /**
+     * <h3> ask </h3>
+     * <p>
+     * Metodo che chiede all'utente se vuole veramente uscire dal gioco.
+     * </p>
+     *
+     * @return Stringa che rappresenta la risposta dell'utente.
+     */
+    public static String ask() {
         Scanner in = new Scanner(System.in, StandardCharsets.UTF_8);
         String command;
         System.out.print("Vuoi veramente uscire? "
@@ -25,13 +50,23 @@ public class ExitBoundary {
         return command;
     }
 
-    public static void operationCancelled()
-    {
+    /**
+     * <h3> operationCancelled </h3>
+     * <p>
+     * Metodo che stampa a video il messaggio di annullamento dell'operazione.
+     * </p>
+     */
+    public static void operationCancelled() {
         System.out.println(Util.RED + "Operazione annullata" + Util.RESET);
     }
 
-    public static void invalidChoice()
-    {
+    /**
+     * <h3> invalidChoice </h3>
+     * <p>
+     * Metodo che stampa a video il messaggio di scelta non valida.
+     * </p>
+     */
+    public static void invalidChoice() {
         System.out.println(Util.RED + "Scelta non valida" + Util.RESET);
     }
 
