41f7904b1295d18ae6c967fb8676e9597d32f80f
Emanuele Fontana <fontanaemanuele14@gmail.com>
2023-05-30 16:32:31 +0200

    Aggiunta JavaDoc per CommandInterface

--- src/main/java/it/uniba/nygaard/game/control/CommandInterface.java
+++ src/main/java/it/uniba/nygaard/game/control/CommandInterface.java
@@ -1,5 +1,18 @@
 package it.uniba.nygaard.game.control;
 
+/**
+ * <h2> CommandInterface </h2>
+ * <p>
+ * L'interfaccia CommandInterface rappresenta un generico comando.
+ * </p>
+ */
 interface CommandInterface {
+    /**
+     * <h3> executeCommand </h3>
+     * <p>
+     *     Esegue il comando.
+     * </p>
+     * @param command Comando da eseguire.
+     */
     void executeCommand(String[] command);
 }
