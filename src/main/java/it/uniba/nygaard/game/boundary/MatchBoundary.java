ded365c1a8376361c59fc0ef6aacc4cebc6a8597
Emanuele Fontana <fontanaemanuele14@gmail.com>
2023-05-30 16:04:06 +0200

    Aggiunto metodo alreadyInGame

--- src/main/java/it/uniba/nygaard/game/boundary/MatchBoundary.java
+++ src/main/java/it/uniba/nygaard/game/boundary/MatchBoundary.java
@@ -9,4 +9,8 @@ public class MatchBoundary {
     {
         System.out.println(Util.RED + "Non puoi iniziare una partita senza impostare il livello di difficoltà" + Util.RESET);
     }
+    public static void alreadyInGame()
+    {
+        System.out.println(Util.RED + "Partita in corso\nPer cominciare una nuova partita abbandona la corrente" + Util.RESET);
+    }
 }
