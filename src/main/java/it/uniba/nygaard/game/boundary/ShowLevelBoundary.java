3dc04f19da9703782fc3274563949bcc1f2390ed
Emanuele Fontana <fontanaemanuele14@gmail.com>
2023-05-30 15:56:16 +0200

    Aggiunto metodo printActualDifficulty

--- src/main/java/it/uniba/nygaard/game/boundary/ShowLevelBoundary.java
+++ src/main/java/it/uniba/nygaard/game/boundary/ShowLevelBoundary.java
@@ -3,4 +3,7 @@ package it.uniba.nygaard.game.boundary;
 public class ShowLevelBoundary {
     private ShowLevelBoundary() {
     }
+    public static void printActualDifficulty(String s) {
+        System.out.println(s);
+    }
 }
