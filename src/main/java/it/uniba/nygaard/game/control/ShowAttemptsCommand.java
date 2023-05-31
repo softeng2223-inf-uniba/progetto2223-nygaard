4137463ffbd75cd6e59e2a52722c934f44aaa7ad
Davide Cirilli <107247984+Davy592@users.noreply.github.com>
2023-05-31 02:39:18 +0200

    Aggiunto metodo executeCommand classe ShowAttemptsCommand

--- src/main/java/it/uniba/nygaard/game/control/ShowAttemptsCommand.java
+++ src/main/java/it/uniba/nygaard/game/control/ShowAttemptsCommand.java
@@ -1,5 +1,9 @@
 package it.uniba.nygaard.game.control;
 
+import it.uniba.nygaard.game.Util;
+import it.uniba.nygaard.game.boundary.ShowAttemptsBoundary;
+import it.uniba.nygaard.game.entity.Match;
+
 public class ShowAttemptsCommand implements Command {
 
     private static final ShowAttemptsCommand instance = new ShowAttemptsCommand();
@@ -10,6 +14,22 @@ public class ShowAttemptsCommand implements Command {
 
     public static ShowAttemptsCommand getInstance() {
         return instance;
+    }
+
+    public void executeCommand(final String[] command) {
+        if (invalidNumber(command)) {
+            return;
+        }
+        Match p = GameManager.getMatch();
+        if (p.getInGame()) {
+            if (p.getAttempts(Util.CUSTOM_DIFFICULTY) != -1) {
+                ShowAttemptsBoundary.showAttempts(p.getUsedAttempts(), p.getFailedAttempts(), p.getAttempts(Util.CUSTOM_DIFFICULTY));
+            } else {
+                ShowAttemptsBoundary.showAttempts(p.getUsedAttempts(), p.getFailedAttempts(), p.getAttempts(p.getDifficulty()));
+            }
+        } else {
+            //TODO: Versione fuori dal gioco
+        }
     }
 
 }
