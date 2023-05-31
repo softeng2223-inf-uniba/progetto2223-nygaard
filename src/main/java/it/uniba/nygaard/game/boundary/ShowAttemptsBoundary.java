6536fdb2c6c5c2488289f030e366dca39caa5805
Davide Cirilli <107247984+Davy592@users.noreply.github.com>
2023-05-31 02:36:32 +0200

    Aggiunto metodo showAttempts classe ShowAttemptsBoundary

--- src/main/java/it/uniba/nygaard/game/boundary/ShowAttemptsBoundary.java
+++ src/main/java/it/uniba/nygaard/game/boundary/ShowAttemptsBoundary.java
@@ -1,8 +1,17 @@
 package it.uniba.nygaard.game.boundary;
 
+import it.uniba.nygaard.game.Util;
+
 public class ShowAttemptsBoundary {
 
     private ShowAttemptsBoundary() {
     }
-    
+
+    public static void showAttempts(int usedAttempts, int failedAttempts, int maxAttempts) {
+        System.out.println("Hai già effettuato " + Util.BOLD + Util.GREEN + usedAttempts + " tentativi " + Util.RESET
+                + "di cui " + Util.BOLD + Util.RED + failedAttempts + " falliti." + Util.RESET);
+        System.out.println("Puoi fallire al massimo " + Util.BOLD + Util.RED + maxAttempts + " tentativi" + Util.RESET
+                + " prima di perdere la partita.");
+    }
+
 }
