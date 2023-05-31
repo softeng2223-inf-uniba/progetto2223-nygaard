668c39333ff875ca4015f316a8716b4769f94dae
Davide Cirilli <107247984+Davy592@users.noreply.github.com>
2023-06-01 00:30:09 +0200

    Creato metodo notInGame classe HitBoundary

--- src/main/java/it/uniba/nygaard/game/boundary/HitBoundary.java
+++ src/main/java/it/uniba/nygaard/game/boundary/HitBoundary.java
@@ -1,8 +1,14 @@
 package it.uniba.nygaard.game.boundary;
 
+import it.uniba.nygaard.game.Util;
+
 public class HitBoundary {
 
     private HitBoundary(){
+    }
+
+    public static void notInGame(){
+        System.out.println(Util.RED + "Non puoi eseguire colpi se non sei in una partita!" + Util.RESET);
     }
 
 }
