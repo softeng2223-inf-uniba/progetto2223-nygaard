db57995a39458549762f00ae12a5c86c475a26e2
Davide Cirilli <107247984+Davy592@users.noreply.github.com>
2023-05-31 02:15:35 +0200

    Aggiunto costruttore classe ShowAttemptsCommand

--- src/main/java/it/uniba/nygaard/game/control/ShowAttemptsCommand.java
+++ src/main/java/it/uniba/nygaard/game/control/ShowAttemptsCommand.java
@@ -1,7 +1,11 @@
 package it.uniba.nygaard.game.control;
 
-public class ShowAttemptsCommand {
+public class ShowAttemptsCommand implements Command {
 
     private static final ShowAttemptsCommand instance = new ShowAttemptsCommand();
+
+    private ShowAttemptsCommand() {
+        setParamNumber(1);
+    }
 
 }
