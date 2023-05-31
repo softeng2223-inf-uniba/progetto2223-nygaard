e28965bf708c241622aed5f155d70fe703e97ce3
Davide Cirilli <107247984+Davy592@users.noreply.github.com>
2023-05-31 02:58:24 +0200

    Aggiunti JavaDoc classe ShowAttemptsCommand

--- src/main/java/it/uniba/nygaard/game/control/ShowAttemptsCommand.java
+++ src/main/java/it/uniba/nygaard/game/control/ShowAttemptsCommand.java
@@ -4,18 +4,51 @@ import it.uniba.nygaard.game.Util;
 import it.uniba.nygaard.game.boundary.ShowAttemptsBoundary;
 import it.uniba.nygaard.game.entity.Match;
 
+/**
+ * << Control >>
+ * <h2> ShowAttemptsCommand </h2>
+ * <p>
+ * La classe ShowAttemptsCommand rappresenta il comando per mostrare il numero di tentativi.
+ * </p>
+ *
+ * @see Command
+ */
 public class ShowAttemptsCommand extends Command {
-
+    /**
+     * <h3> instance </h3>
+     * <p>
+     *     Istanza di ShowAttemptsCommand.
+     * </p>
+     */
     private static final ShowAttemptsCommand instance = new ShowAttemptsCommand();
 
+    /**
+     * <h3> Costruttore </h3>
+     * <p>
+     *     Costruttore della classe ShowAttemptsCommand.
+     * </p>
+     */
     private ShowAttemptsCommand() {
         setParamNumber(1);
     }
 
+    /**
+     * <h3> getInstance </h3>
+     * <p>
+     *    Restituisce l'istanza di ShowAttemptsCommand.
+     * </p>
+     */
     public static ShowAttemptsCommand getInstance() {
         return instance;
     }
 
+    /**
+     * <h3> executeCommand </h3>
+     * <p>
+     *     Esegue il comando per mostrare il numero di tentativi.
+     * </p>
+     * @param command comando da eseguire
+     */
     public void executeCommand(final String[] command) {
         if (invalidNumber(command)) {
             return;
