61b59d31cf6290e508e402aa8b901228936c9cbf
Davide Cirilli <107247984+Davy592@users.noreply.github.com>
2023-05-31 02:52:21 +0200

    Aggiunti JavaDoc classe ShowAttemptsBoundary

--- src/main/java/it/uniba/nygaard/game/boundary/ShowAttemptsBoundary.java
+++ src/main/java/it/uniba/nygaard/game/boundary/ShowAttemptsBoundary.java
@@ -2,11 +2,34 @@ package it.uniba.nygaard.game.boundary;
 
 import it.uniba.nygaard.game.Util;
 
+/**
+ * <<Boundary>>
+ * <h2> ShowAttemptsBoundary </h2>
+ * <p>
+ * La classe ShowAttemptsBoundary stampa il numero di tentativi
+ * effettuati, falliti e massimi fallibili dall'utente.
+ * </p>
+ */
 public class ShowAttemptsBoundary {
-
+    /**
+     * <h3> Costruttore </h3>
+     * <p>
+     * Costruttore della classe ShowAttemptsBoundary.
+     * </p>
+     */
     private ShowAttemptsBoundary() {
     }
 
+    /**
+     * <h3> showAttempts </h3>
+     * <p>
+     * Stampa il numero di tentativi effettuati, falliti e massimi fallibili dall'utente.
+     * </p>
+     *
+     * @param usedAttempts   Tentativi effettuati
+     * @param failedAttempts Tentativi falliti
+     * @param maxAttempts    Tentativi massimi fallibili
+     */
     public static void showAttempts(int usedAttempts, int failedAttempts, int maxAttempts) {
         System.out.println("Hai già effettuato " + Util.BOLD + Util.GREEN + usedAttempts + " tentativi " + Util.RESET
                 + "di cui " + Util.BOLD + Util.RED + failedAttempts + " falliti." + Util.RESET);
