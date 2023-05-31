dfed3574f63f30c18725da9bc1e0356a79faa256
Stefano-Labianca <lstefano@protonmail.com>
2023-05-31 16:38:06 +0200

    Aggiunto nuovo controllo
    Aggiunto un controllo extra per controllare se l'utente stia
    inserendo due volte le stesse dimensioni

--- src/main/java/it/uniba/nygaard/game/control/SetGridSizeCommand.java
+++ src/main/java/it/uniba/nygaard/game/control/SetGridSizeCommand.java
@@ -1,5 +1,6 @@
 package it.uniba.nygaard.game.control;
 
+import it.uniba.nygaard.game.Util;
 import it.uniba.nygaard.game.boundary.GridSizeBoundary;
 
 /**
@@ -30,7 +31,34 @@ final public class SetGridSizeCommand extends Command {
             return;
         }
 
+        int sizeCommand = this.getMappedCommand(command[0]);
+        int currentSize = GameManager.getGridSize();
+
+        if (sizeCommand == currentSize) {
+            GridSizeBoundary.sameSizeError();
+            return;
+        }
+
+
+        //TODO: Aggiungere messaggio per segnalare il cambio di dimensioni
+        // e chiede la conferma
+
+
+
+
         //TODO: Imposta dimensioni
         GridSizeBoundary.operationDone();
+    }
+
+    private int getMappedCommand(String command) {
+        int size = Util.STANDARD_GRID_SIZE;
+
+        switch (command) {
+            case "/standard" -> size = Util.STANDARD_GRID_SIZE;
+            case "/large" -> size = Util.LARGE_GRID_SIZE;
+            case "/extralarge" -> size = Util.EXTRA_LARGE_GRID_SIZE;
+        }
+
+        return size;
     }
 }
