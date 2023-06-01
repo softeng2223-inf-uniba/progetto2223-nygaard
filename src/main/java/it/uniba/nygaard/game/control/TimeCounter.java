44c4ab43961d8755f2bfa7cd70989a9b8b718208
Davide Cirilli <107247984+Davy592@users.noreply.github.com>
2023-06-01 18:22:19 +0200

    Aggiunti JavaDoc classe TimeCounter

--- src/main/java/it/uniba/nygaard/game/control/TimeCounter.java
+++ src/main/java/it/uniba/nygaard/game/control/TimeCounter.java
@@ -4,7 +4,23 @@ import it.uniba.nygaard.game.Util;
 import it.uniba.nygaard.game.boundary.MatchBoundary;
 import it.uniba.nygaard.game.entity.Match;
 
+/**
+ * << Control >>
+ * <h2> TimeCounter </h2>
+ * <p>
+ * La classe TimeCounter rappresenta il contatore del tempo di gioco.
+ * </p>
+ *
+ * @see Thread
+ */
 class TimeCounter extends Thread {
+
+  /**
+   * <h3> run </h3>
+   * <p>
+   * Avvia il contatore del tempo di gioco.
+   * </p>
+   */
   public void run() {
     Match p = GameManager.getMatch();
     while (true) {
