20f134cfab89b824cc572b5636b8f1658bc30fcd
Davide Cirilli <107247984+Davy592@users.noreply.github.com>
2023-06-01 17:55:51 +0200

    Risolti warnings CheckStyle classe TimeCounter

--- src/main/java/it/uniba/nygaard/game/control/TimeCounter.java
+++ src/main/java/it/uniba/nygaard/game/control/TimeCounter.java
@@ -2,12 +2,14 @@ package it.uniba.nygaard.game.control;
 
 import it.uniba.nygaard.game.Util;
 import it.uniba.nygaard.game.boundary.MatchBoundary;
+import it.uniba.nygaard.game.entity.Match;
 
 class TimeCounter extends Thread {
   public void run() {
+    Match p = GameManager.getMatch();
     while (true) {
-      if (GameManager.getMatch().getInGame()) {
-        if (System.currentTimeMillis() - GameManager.getMatch().getStartTime() > GameManager.getMatch().getMaxTime() * Util.ONE_MINUTE) {
+      if (p.getInGame()) {
+        if (System.currentTimeMillis() - p.getStartTime() > p.getMaxTime() * Util.ONE_MINUTE) {
           GeneralControl.setShutDown(4);
           MatchBoundary.timeOut();
           break;
