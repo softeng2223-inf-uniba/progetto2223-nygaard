552422716dd0bb759b3a2fc672766f995c2dc18e
Davide Cirilli <107247984+Davy592@users.noreply.github.com>
2023-06-01 18:21:31 +0200

    Aggiunti JavaDoc classe HitCommand

--- src/main/java/it/uniba/nygaard/game/control/HitCommand.java
+++ src/main/java/it/uniba/nygaard/game/control/HitCommand.java
@@ -8,18 +8,55 @@ import it.uniba.nygaard.game.boundary.ShowAttemptsBoundary;
 import it.uniba.nygaard.game.boundary.ShowGridBoundary;
 import it.uniba.nygaard.game.entity.Match;
 
+/**
+ * << Control >>
+ * <h2> HitCommand </h2>
+ * <p>
+ * La classe HitCommand rappresenta il comando per colpire una nave.
+ * </p>
+ *
+ * @see Command
+ */
 final class HitCommand extends Command {
 
+  /**
+   * <h3> instance </h3>
+   * <p>
+   * Istanza di HitCommand.
+   * </p>
+   */
   private static HitCommand instance = new HitCommand();
 
+  /**
+   * <h3> Costruttore </h3>
+   * <p>
+   * Costruttore della classe HitCommand.
+   * </p>
+   */
   private HitCommand() {
     setParamNumber(1);
   }
 
+  /**
+   * <h3> getInstance </h3>
+   * <p>
+   * Restituisce l'istanza di HitCommand.
+   * </p>
+   */
   static HitCommand getInstance() {
     return instance;
   }
 
+  /**
+   * <h3> invalidNumber </h3>
+   * <p>
+   * Controlla se il numero di parametri è valido.
+   * </p>
+   *
+   * @param command comando da eseguire
+   * @param params  parametri del comando
+   * @return true se il numero di parametri è invalido, false altrimenti
+   */
   boolean invalidNumber(final String[] command, final String... params) {
     if (command.length > this.getParamNumber()) {
       InputBoundary.howToUse("<lettera>-<numero>");
@@ -28,6 +65,14 @@ final class HitCommand extends Command {
     return false;
   }
 
+  /**
+   * <h3> executeCommand </h3>
+   * <p>
+   * Esegue il comando per colpire una nave.
+   * </p>
+   *
+   * @param command comando da eseguire
+   */
   public void executeCommand(final String[] command) {
     if (invalidNumber(command)) {
       return;
