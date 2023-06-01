0a98a4a20854f9a153f2ae8b8762fb54040a6b82
Davide Cirilli <107247984+Davy592@users.noreply.github.com>
2023-06-01 16:55:09 +0200

    Aggiunta stampa per tempo terminato

--- src/main/java/it/uniba/nygaard/game/control/TimeCounter.java
+++ src/main/java/it/uniba/nygaard/game/control/TimeCounter.java
@@ -1,18 +1,18 @@
 package it.uniba.nygaard.game.control;
 
 import it.uniba.nygaard.game.Util;
-import it.uniba.nygaard.game.control.GameManager;
-import it.uniba.nygaard.game.control.GeneralControl;
+import it.uniba.nygaard.game.boundary.MatchBoundary;
 
 class TimeCounter extends Thread {
   public void run() {
     while (true) {
       if (GameManager.getMatch().getInGame()) {
-        if(System.currentTimeMillis() - GameManager.getMatch().getStartTime() > GameManager.getMatch().getMaxTime() * Util.ONE_MINUTE){
-          GeneralControl.setShutDown(3);
+        if (System.currentTimeMillis() - GameManager.getMatch().getStartTime() > GameManager.getMatch().getMaxTime() * Util.ONE_MINUTE) {
+          GeneralControl.setShutDown(4);
+          MatchBoundary.timeOut();
           break;
         }
-      }else{
+      } else {
         break;
       }
     }
