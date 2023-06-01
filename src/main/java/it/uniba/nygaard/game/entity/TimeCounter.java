c1ba1cc6834fa7e3f9bed88587bbfdcb1eb6776c
Davide <davide.cirilli592@gmail.com>
2023-06-01 10:09:33 +0200

    Creata classe TimeCounter

---
+++ src/main/java/it/uniba/nygaard/game/entity/TimeCounter.java
@@ -0,0 +1,8 @@
+package it.uniba.nygaard.game.entity;
+
+import it.uniba.nygaard.game.Util;
+import it.uniba.nygaard.game.control.GameManager;
+import it.uniba.nygaard.game.control.GeneralControl;
+
+public class TimeCounter implements Runnable{
+}
