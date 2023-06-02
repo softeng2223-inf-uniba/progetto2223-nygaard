307df19959e671c26e23b91670277ca19da1b338
GiudittaIzzo <izzogiuditta.2e@gmail.com>
2023-06-02 19:35:48 +0200

    Aggiunto javadoc della classe ShowGridCommand

--- src/main/java/it/uniba/nygaard/game/control/ShowGridCommand.java
+++ src/main/java/it/uniba/nygaard/game/control/ShowGridCommand.java
@@ -3,6 +3,15 @@ package it.uniba.nygaard.game.control;
 import it.uniba.nygaard.game.boundary.ShowGridBoundary;
 import it.uniba.nygaard.game.entity.Match;
 
+/**
+ * << Control >>
+ * <h2> ShowGridCommand </h2>
+ * <p>
+ * La classe ShowGridCommand rappresenta il comando per mostrare la griglia di attacco.
+ * </p>
+ *
+ * @see Command
+ */
 final class ShowGridCommand extends Command {
 
   private static ShowGridCommand instance = new ShowGridCommand();
