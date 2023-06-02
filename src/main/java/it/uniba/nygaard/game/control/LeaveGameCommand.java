d1a4d89321ae423f8302e390d56bc75114e77a07
GiudittaIzzo <izzogiuditta.2e@gmail.com>
2023-06-02 17:07:02 +0200

    Aggiunti javadoc in 'LeaveGameCommand'

--- src/main/java/it/uniba/nygaard/game/control/LeaveGameCommand.java
+++ src/main/java/it/uniba/nygaard/game/control/LeaveGameCommand.java
@@ -1,29 +1,46 @@
 package it.uniba.nygaard.game.control;
 
 import it.uniba.nygaard.game.Util;
-import it.uniba.nygaard.game.boundary.ExitBoundary;
 import it.uniba.nygaard.game.boundary.LeaveGameBoundary;
-import it.uniba.nygaard.game.boundary.MatchBoundary;
 
 public class LeaveGameCommand extends Command {
+
   /**
-   *
+   * <h3> instance </h3>
+   * <p>
+   * Istanza di LeaveGameCommand.
+   * </p>
    */
   private static LeaveGameCommand instance = new LeaveGameCommand();
 
   /**
-   *
+   * <h2> Costruttore </h2>
+   * <p>
+   * Costruttore  della classe LeaveGameCommand.
+   * </p>
    */
   private LeaveGameCommand() {
     setParamNumber(1);
   }
 
   /**
-   *
+   * <h2> getInstance </h2>
+   * <p>
+   * Restituisce l'istanza di LeaveGameCommand.
+   * </p>
    */
   public static LeaveGameCommand getInstance() {
     return instance;
   }
+
+  /**
+   * <h2> executeCommand </h2>
+   * <p>
+   * Esegue il comando per decidere se abbandonare la partita.
+   * </p>
+   *
+   * @param command comando da eseguire
+   */
 
   void executeCommand(final String[] command) {
     if (!GameManager.getMatch().getInGame()) {
