529cae77a14809a4dc5e41e6563b5dac2fbbd948
GiudittaIzzo <izzogiuditta.2e@gmail.com>
2023-06-02 16:48:50 +0200

    Aggiunto javadoc del metodo 'invalidChoice' in 'LeaveGameBoundary'

--- src/main/java/it/uniba/nygaard/game/boundary/LeaveGameBoundary.java
+++ src/main/java/it/uniba/nygaard/game/boundary/LeaveGameBoundary.java
@@ -23,6 +23,12 @@ public class LeaveGameBoundary {
         System.out.println(Util.RED + "Non puoi usare il comando quando non sei in partita"
             + Util.RESET);
     }
+    /**
+     * <h2> invalidChoice </h2>
+     * <p>
+     * Messaggio di errore per quando si inserisce una scelta non valida.
+     * </p>
+     */
     public static void invalidChoice() {
         System.out.println(Util.RED + "Scelta non valida" + Util.RESET);
     }
