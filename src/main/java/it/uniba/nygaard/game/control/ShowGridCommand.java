38e356cfd5907c3745c895157193c9e5a66e0b64
GiudittaIzzo <izzogiuditta.2e@gmail.com>
2023-06-02 19:33:27 +0200

    Creato scheletro della classe 'ShowGridCommand'

--- src/main/java/it/uniba/nygaard/game/control/ShowGridCommand.java
+++ src/main/java/it/uniba/nygaard/game/control/ShowGridCommand.java
@@ -4,4 +4,15 @@ import it.uniba.nygaard.game.boundary.ShowGridBoundary;
 import it.uniba.nygaard.game.entity.Match;
 
 final class ShowGridCommand extends Command {
+
+  private static ShowGridCommand instance = new ShowGridCommand();
+
+  private ShowGridCommand() {
+    setParamNumber(1);
+  }
+
+  static ShowGridCommand getInstance() {
+    return instance;
+  }
+
 }
