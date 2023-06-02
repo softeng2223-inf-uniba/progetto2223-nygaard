9293f6f37467bfc737a70fc88d6c3095161b3153
GiudittaIzzo <izzogiuditta.2e@gmail.com>
2023-06-02 16:28:28 +0200

    Implementato metodo 'executeCommand' in 'LeaveGameCommand'

--- src/main/java/it/uniba/nygaard/game/control/LeaveGameCommand.java
+++ src/main/java/it/uniba/nygaard/game/control/LeaveGameCommand.java
@@ -1,26 +1,54 @@
 package it.uniba.nygaard.game.control;
 
+import it.uniba.nygaard.game.Util;
+import it.uniba.nygaard.game.boundary.ExitBoundary;
 import it.uniba.nygaard.game.boundary.LeaveGameBoundary;
+import it.uniba.nygaard.game.boundary.MatchBoundary;
 
 public class LeaveGameCommand extends Command {
-    /**
-     */
-    private static LeaveGameCommand instance = new LeaveGameCommand();
+  /**
+   *
+   */
+  private static LeaveGameCommand instance = new LeaveGameCommand();
 
-    /**
-     */
-    private LeaveGameCommand() {
-        setParamNumber(1);
+  /**
+   *
+   */
+  private LeaveGameCommand() {
+    setParamNumber(1);
+  }
+
+  /**
+   *
+   */
+  public static LeaveGameCommand getInstance() {
+    return instance;
+  }
+
+  void executeCommand(final String[] command) {
+    if (!GameManager.getMatch().getInGame()) {
+      LeaveGameBoundary.notInGame();
+      return;
     }
-
-    /**
-     */
-    public static LeaveGameCommand getInstance() {
-        return instance;
+    if (invalidNumber(command)) {
+      return;
     }
-
-    void executeCommand(String[] command) {
-
-    }
+    String choice;
+    do {
+      choice = LeaveGameBoundary.ask();
+      if (GeneralControl.getShutDown() != Util.NOT_TERMINATION_CODE) {
+        return;
+      }
+      if (choice.equals("y")) {
+        //MatchBoundary.leave();
+        GeneralControl.setShutDown(Util.LEFT_TERMINATION_CODE);
+        return;
+      }
+      if (choice.equals("n")) {
+        LeaveGameBoundary.operationCancelled();
+      } else {
+        LeaveGameBoundary.invalidChoice();
+      }
+    } while (!choice.equals("n"));
+  }
 }
-
