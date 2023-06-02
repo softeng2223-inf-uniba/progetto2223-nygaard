bcbb8f37c98403ce2b50e49b9cea40c7183c40bd
GiudittaIzzo <izzogiuditta.2e@gmail.com>
2023-06-02 16:43:58 +0200

    Creato costruttore di 'LeaveGameBoundary'

--- src/main/java/it/uniba/nygaard/game/boundary/LeaveGameBoundary.java
+++ src/main/java/it/uniba/nygaard/game/boundary/LeaveGameBoundary.java
@@ -1,3 +1,12 @@
 package it.uniba.nygaard.game.boundary;
+
+import it.uniba.nygaard.game.Util;
+import java.nio.charset.StandardCharsets;
+import java.util.Scanner;
+
 public class LeaveGameBoundary {
+
+    private LeaveGameBoundary() {
+    }
+
 }
