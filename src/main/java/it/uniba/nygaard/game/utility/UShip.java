e57721565764a508a77c5ae82065f04f869d4710
mattiacurri <mattia2002@gmail.com>
2023-06-06 13:36:47 +0200

    Creata classe UShip

---
+++ src/main/java/it/uniba/nygaard/game/utility/UShip.java
@@ -0,0 +1,35 @@
+package it.uniba.nygaard.game.utility;
+
+/**
+ * <h2> UColor </h2>
+ * <p>
+ *   La classe UColor contiene delle costanti utili
+ *   per la gestione dei colori.
+ * </p>
+ *
+ */
+public final class UShip {
+  public static final int MIN_SHIP = 1;
+  public static final int MAX_SHIP = 10;
+  public static final int AIRCRAFT_HP = 5;
+  public static final int BATTLESHIP_HP = 4;
+  public static final int CRUISER_HP = 3;
+  public static final int DESTROYER_HP = 2;
+  public static final int AIRCRAFT_NO = 1;
+  public static final int BATTLESHIP_NO = 2;
+  public static final int CRUISER_NO = 3;
+  public static final int DESTROYER_NO = 4;
+  public static final int MIN_GENERATIONS = 1;
+  public static final int MAX_GENERATIONS = 50;
+  public static final boolean VERTICAL = true;
+  public static final boolean HORIZONTAL = false;
+
+  /**
+   * <h3> Costruttore </h3>
+   * <p>
+   * Costruttore della classe UShip.
+   * </p>
+   */
+  private UShip() {
+  }
+}
