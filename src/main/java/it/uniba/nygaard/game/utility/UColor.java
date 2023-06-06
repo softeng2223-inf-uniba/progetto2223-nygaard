9d7642edc0281ddc42cb0e2ac0ecc7fac934f45c
mattiacurri <mattia2002@gmail.com>
2023-06-06 13:11:37 +0200

    Creata classe UColor

---
+++ src/main/java/it/uniba/nygaard/game/utility/UColor.java
@@ -0,0 +1,30 @@
+package it.uniba.nygaard.game.utility;
+
+/**
+ * <h2> UColor </h2>
+ * <p>
+ *   La classe UColor contiene delle costanti utili
+ *   per la gestione dei colori.
+ * </p>
+ *
+ */
+public final class UColor {
+  public static final String RED = "\u001B[31m";
+  public static final String GREEN = "\u001B[32m";
+  public static final String YELLOW = "\u001B[33m";
+  public static final String BLUE = "\u001B[34m";
+  public static final String PURPLE = "\u001B[35m";
+  public static final String CYAN = "\u001B[36m";
+  public static final String RESET = "\u001B[0m";
+  public static final String BOLD = "\u001B[1m";
+  public static final String ITALIC = "\u001B[3m";
+
+  /**
+   * <h3> Costruttore </h3>
+   * <p>
+   * Costruttore della classe UColor.
+   * </p>
+   */
+  private UColor() {
+  }
+}
