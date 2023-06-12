08faf611165435d2b31255fc57dad8173676f867
Davide Cirilli <107247984+Davy592@users.noreply.github.com>
2023-06-13 00:18:42 +0200

    Aggiunto test /mostratempo troppi argomenti
    Co-authored-by: Mattia Curri <mattia2002@gmail.com>

--- src/test/java/it/uniba/nygaard/game/control/ShowTimeCommandTest.java
+++ src/test/java/it/uniba/nygaard/game/control/ShowTimeCommandTest.java
@@ -27,4 +27,19 @@ class ShowTimeCommandTest {
     System.setOut(new PrintStream(outContent, true, StandardCharsets.UTF_8));
     GameManager.getMatch().setInGame(true);
   }
+
+  @Test
+  @DisplayName("/mostratempo con troppi argomenti")
+  void testShowTimeCommandWithTooManyArgument() {
+    String[] args = new String[]{"/mostratempo", "stringa"};
+    try {
+      execute.invoke(showTimeCommand, (Object) args);
+    } catch (IllegalAccessException | InvocationTargetException e) {
+      fail("Impossibile invocare il metodo executeCommand");
+    }
+    String expectedOutput = UColor.CYAN + "Il comando dovrebbe essere usato come segue: /mostratempo" + UColor.RESET
+        + System.lineSeparator();
+    assertEquals(expectedOutput, outContent.toString(StandardCharsets.UTF_8),
+        "Non è stato stampato il messaggio di errore corretto");
+  }
 }
