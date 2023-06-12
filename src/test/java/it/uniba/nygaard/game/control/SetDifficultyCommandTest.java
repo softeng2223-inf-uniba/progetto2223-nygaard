120fc758ae10bbbdae3a83c62d9ac14ab4146331
Davide Cirilli <107247984+Davy592@users.noreply.github.com>
2023-06-12 23:45:32 +0200

    Aggiunti attributi classe SetDifficultyCommandTest
    Co-authored-by: Mattia Curri <mattia2002@gmail.com>

--- src/test/java/it/uniba/nygaard/game/control/SetDifficultyCommandTest.java
+++ src/test/java/it/uniba/nygaard/game/control/SetDifficultyCommandTest.java
@@ -1,4 +1,10 @@
 package it.uniba.nygaard.game.control;
 
 class SetDifficultyCommandTest {
+
+  private static SetDifficultyCommand setDifficultyCommand;
+  private static Method execute;
+  private static ByteArrayOutputStream outContent;
+  private static InputStream inBackup;
+
 }
