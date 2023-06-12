80b74076fe50203a3c9397815d1c2066634b2f85
Davide Cirilli <107247984+Davy592@users.noreply.github.com>
2023-06-13 00:18:20 +0200

    Aggiunti metodi @Before classe ShowTimeCommandTest
    Co-authored-by: Mattia Curri <mattia2002@gmail.com>

--- src/test/java/it/uniba/nygaard/game/control/ShowTimeCommandTest.java
+++ src/test/java/it/uniba/nygaard/game/control/ShowTimeCommandTest.java
@@ -6,4 +6,25 @@ class ShowTimeCommandTest {
   private static Method execute;
   private static ByteArrayOutputStream outContent;
 
+  @BeforeAll
+  static void setUpAll() throws NoSuchMethodException {
+    outContent = new ByteArrayOutputStream();
+    try {
+      GameManager.setMatch(new Match());
+      Method getInstance = ShowTimeCommand.class.getDeclaredMethod("getInstance");
+      getInstance.setAccessible(true);
+      showTimeCommand = (ShowTimeCommand) getInstance.invoke(null);
+      execute = showTimeCommand.getClass().getDeclaredMethod("executeCommand", String[].class);
+      execute.setAccessible(true);
+    } catch (NoSuchMethodException | IllegalAccessException | InvocationTargetException e) {
+      fail("Impossibile testare il comando /mostratempo");
+    }
+  }
+
+  @BeforeEach
+  void setUp() {
+    outContent.reset();
+    System.setOut(new PrintStream(outContent, true, StandardCharsets.UTF_8));
+    GameManager.getMatch().setInGame(true);
+  }
 }
