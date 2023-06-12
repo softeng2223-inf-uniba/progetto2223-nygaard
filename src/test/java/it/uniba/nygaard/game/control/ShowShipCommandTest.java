ae0275d4848850e05b403090e541ae3690da51a5
Davide Cirilli <107247984+Davy592@users.noreply.github.com>
2023-06-13 00:16:40 +0200

    Aggiunto test /mostranavi corretto
    Co-authored-by: Mattia Curri <mattia2002@gmail.com>

--- src/test/java/it/uniba/nygaard/game/control/ShowShipCommandTest.java
+++ src/test/java/it/uniba/nygaard/game/control/ShowShipCommandTest.java
@@ -40,4 +40,29 @@ class ShowShipCommandTest {
     assertEquals(expectedOutput, outContent.toString(StandardCharsets.UTF_8),
         "Non è stato stampato il messaggio di errore corretto");
   }
+
+  @Test
+  @DisplayName("/mostranavi corretto")
+  void testShowShipCommandCorrect() {
+    String[] args = new String[]{"/mostranavi"};
+    try {
+      execute.invoke(showShipCommand, (Object) args);
+    } catch (Exception e) {
+      fail("Impossibile invocare il metodo executeCommand");
+    }
+    String expectedOutput = UColor.CYAN + UColor.ITALIC + "\tCacciatorpediniere \t" + UColor.RESET
+        + "■■" + "\t" + UColor.CYAN + UColor.BOLD
+        + "Esemplari: " + UShip.DESTROYER_NO + UColor.RESET + System.lineSeparator()
+        + UColor.CYAN + UColor.ITALIC + "\tIncrociatore \t\t" + UColor.RESET
+        + "■■■" + "\t" + UColor.CYAN + UColor.BOLD
+        + "Esemplari: " + UShip.CRUISER_NO + UColor.RESET + System.lineSeparator()
+        + UColor.CYAN + UColor.ITALIC + "\tCorazzata \t\t" + UColor.RESET
+        + "■■■■" + "\t" + UColor.CYAN + UColor.BOLD
+        + "Esemplari: " + UShip.BATTLESHIP_NO + UColor.RESET + System.lineSeparator()
+        + UColor.CYAN + UColor.ITALIC + "\tPortaerei \t\t" + UColor.RESET
+        + "■■■■■" + "\t" + UColor.CYAN + UColor.BOLD
+        + "Esemplari: " + UShip.AIRCRAFT_NO + UColor.RESET + System.lineSeparator();
+    assertEquals(expectedOutput, outContent.toString(StandardCharsets.UTF_8),
+        "Non è stato stampato il messaggio corretto");
+  }
 }
