4a118e11dbfffbc3a5e7b6e244d864159e3a52be
Davide Cirilli <107247984+Davy592@users.noreply.github.com>
2023-06-12 23:56:25 +0200

    Aggiunti attributi classe SetGridSizeCommandTest
    Co-authored-by: Mattia Curri <mattia2002@gmail.com>

--- src/test/java/it/uniba/nygaard/game/control/SetGridSizeCommandTest.java
+++ src/test/java/it/uniba/nygaard/game/control/SetGridSizeCommandTest.java
@@ -1,4 +1,10 @@
 package it.uniba.nygaard.game.control;
 
 class SetGridSizeCommandTest {
+
+  private static SetGridSizeCommand setGridSizeCommand;
+  private static Method execute;
+  private static ByteArrayOutputStream outContent;
+  private static InputStream inBackup;
+  
 }
