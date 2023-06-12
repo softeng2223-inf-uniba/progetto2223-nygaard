eabd91357a314ce4ea184ecac1fdac6a81d600a8
Davide Cirilli <107247984+Davy592@users.noreply.github.com>
2023-06-12 23:29:51 +0200

    Aggiunti metodi @Before classe HelpCommandTest
    Co-authored-by: Mattia Curri <mattia2002@gmail.com>

--- src/test/java/it/uniba/nygaard/game/control/HelpCommandTest.java
+++ src/test/java/it/uniba/nygaard/game/control/HelpCommandTest.java
@@ -6,4 +6,23 @@ class HelpCommandTest {
   private static Method execute;
   private static ByteArrayOutputStream outContent;
 
+  @BeforeAll
+  static void setUpAll() {
+    outContent = new ByteArrayOutputStream();
+    try {
+      Method getInstance = HelpCommand.class.getDeclaredMethod("getInstance");
+      getInstance.setAccessible(true);
+      helpCommand = (HelpCommand) getInstance.invoke(null);
+      execute = helpCommand.getClass().getDeclaredMethod("executeCommand", String[].class);
+      execute.setAccessible(true);
+    } catch (InvocationTargetException | NoSuchMethodException | IllegalAccessException e) {
+      fail("Impossibile testare il comando /help");
+    }
+  }
+
+  @BeforeEach
+  void setUp() {
+    outContent.reset();
+    System.setOut(new PrintStream(outContent, true, StandardCharsets.UTF_8));
+  }
 }
