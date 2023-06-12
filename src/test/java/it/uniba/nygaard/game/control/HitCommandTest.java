e05443e3710942fa173744b5cd7bc1d5989969e7
Davide Cirilli <107247984+Davy592@users.noreply.github.com>
2023-06-12 23:32:53 +0200

    Aggiunto test colpo non in partita
    Co-authored-by: Mattia Curri <mattia2002@gmail.com>

--- src/test/java/it/uniba/nygaard/game/control/HitCommandTest.java
+++ src/test/java/it/uniba/nygaard/game/control/HitCommandTest.java
@@ -44,4 +44,20 @@ class HitCommandTest {
     assertEquals(expectedOutput, outContent.toString(StandardCharsets.UTF_8),
         "Non è stato stampato il messaggio di errore corretto");
   }
+
+  @Test
+  @DisplayName("Colpo non in partita")
+  void testHitCommandNotInGame() {
+    String[] args = new String[]{"B-4"};
+    GameManager.getMatch().setInGame(false);
+    try {
+      execute.invoke(hitCommand, (Object) args);
+    } catch (IllegalAccessException | InvocationTargetException e) {
+      fail("Impossibile invocare il metodo executeCommand");
+    }
+    String expectedOutput = UColor.RED + "Non puoi eseguire colpi se non sei in partita"
+        + UColor.RESET + System.lineSeparator();
+    assertEquals(expectedOutput, outContent.toString(StandardCharsets.UTF_8),
+        "Non è stato stampato il messaggio di errore corretto");
+  }
 }
