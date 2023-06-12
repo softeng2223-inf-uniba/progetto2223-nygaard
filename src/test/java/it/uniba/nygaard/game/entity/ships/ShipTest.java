25cb38ce37231ebe862ccb369dc1c28cf3725332
Davide Cirilli <107247984+Davy592@users.noreply.github.com>
2023-06-13 00:31:37 +0200

    Aggiunti attributi classe ShipTest
    Co-authored-by: Mattia Curri <mattia2002@gmail.com>

--- src/test/java/it/uniba/nygaard/game/entity/ships/ShipTest.java
+++ src/test/java/it/uniba/nygaard/game/entity/ships/ShipTest.java
@@ -1,4 +1,10 @@
 package it.uniba.nygaard.game.entity.ships;
 
 class ShipTest {
+
+  private Ship ship;
+  private CellsGrid grid;
+  private static Coordinate coord;
+  private static Random rnd;
+
 }
