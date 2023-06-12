a745a580fc7e5fcf60f84f82f2208c807f504ef2
Davide Cirilli <107247984+Davy592@users.noreply.github.com>
2023-06-13 00:10:48 +0200

    Aggiunti metodi @Before classe ShowGridCommandTest
    Co-authored-by: Mattia Curri <mattia2002@gmail.com>

--- src/test/java/it/uniba/nygaard/game/control/ShowGridCommandTest.java
+++ src/test/java/it/uniba/nygaard/game/control/ShowGridCommandTest.java
@@ -6,4 +6,26 @@ class ShowGridCommandTest {
   private static Method execute;
   private static ByteArrayOutputStream outContent;
 
+  @BeforeAll
+  static void setUpAll() {
+    outContent = new ByteArrayOutputStream();
+    try {
+      GameManager.setMatch(new Match());
+      Method getInstance = ShowGridCommand.class.getDeclaredMethod("getInstance");
+      getInstance.setAccessible(true);
+      revealGridCommand = (ShowGridCommand) getInstance.invoke(null);
+      execute = revealGridCommand.getClass().getDeclaredMethod("executeCommand", String[].class);
+      execute.setAccessible(true);
+    } catch (InvocationTargetException | NoSuchMethodException | IllegalAccessException e) {
+      fail("Impossibile testare i comandi /mostragriglia e /svelagriglia");
+    }
+  }
+
+  @BeforeEach
+  void setUp() {
+    outContent.reset();
+    System.setOut(new PrintStream(outContent, true, StandardCharsets.UTF_8));
+    GameManager.getMatch().setInGame(true);
+  }
+
 }
