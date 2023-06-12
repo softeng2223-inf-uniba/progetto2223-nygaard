3ee8a212b53363cf38eb23a7d3d04eafd5de96af
Davide Cirilli <107247984+Davy592@users.noreply.github.com>
2023-06-12 23:18:02 +0200

    Aggiunti metodi @Before classe AttemptsCommandTest
    Co-authored-by: Mattia Curri <mattia2002@gmail.com>

--- src/test/java/it/uniba/nygaard/game/control/AttemptsCommandTest.java
+++ src/test/java/it/uniba/nygaard/game/control/AttemptsCommandTest.java
@@ -6,4 +6,25 @@ class AttemptsCommandTest {
   private static Method execute;
   private static ByteArrayOutputStream outContent;
 
+  @BeforeAll
+  static void setUpAll() {
+    outContent = new ByteArrayOutputStream();
+    try {
+      GameManager.setMatch(new Match());
+      Method getInstance = AttemptsCommand.class.getDeclaredMethod("getInstance");
+      getInstance.setAccessible(true);
+      attemptsCommand = (AttemptsCommand) getInstance.invoke(null);
+      execute = attemptsCommand.getClass().getDeclaredMethod("executeCommand", String[].class);
+      execute.setAccessible(true);
+    } catch (InvocationTargetException | NoSuchMethodException | IllegalAccessException e) {
+      fail("Impossibile testare il comando /tentativi");
+    }
+  }
+
+  @BeforeEach
+  void setUp() {
+    outContent.reset();
+    System.setOut(new PrintStream(outContent, true, StandardCharsets.UTF_8));
+    GameManager.getMatch().setInGame(false);
+  }
 }
