594d582cd2f12ddaf789a5fa6d9e7960008eae83
Davide Cirilli <107247984+Davy592@users.noreply.github.com>
2023-06-12 23:36:05 +0200

    Creati attributi classe LeaveMatchCommandTest
    Co-authored-by: Mattia Curri <mattia2002@gmail.com>

--- src/test/java/it/uniba/nygaard/game/control/LeaveMatchCommandTest.java
+++ src/test/java/it/uniba/nygaard/game/control/LeaveMatchCommandTest.java
@@ -1,4 +1,9 @@
 package it.uniba.nygaard.game.control;
 
 class LeaveMatchCommandTest {
+
+  private static LeaveMatchCommand leaveMatchCommand;
+  private static Method execute;
+  private static ByteArrayOutputStream outContent;
+  private static InputStream inBackup;
 }
