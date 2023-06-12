dc1c5359198af40d4c03a4879a4ca28ae5624105
Davide Cirilli <107247984+Davy592@users.noreply.github.com>
2023-06-13 00:16:07 +0200

    Aggiunti metodi @Before classe ShowShipCommandTest
    Co-authored-by: Mattia Curri <mattia2002@gmail.com>

--- src/test/java/it/uniba/nygaard/game/control/ShowShipCommandTest.java
+++ src/test/java/it/uniba/nygaard/game/control/ShowShipCommandTest.java
@@ -6,4 +6,23 @@ class ShowShipCommandTest {
   private static Method execute;
   private static ByteArrayOutputStream outContent;
 
+  @BeforeAll
+  static void setUpAll() {
+    outContent = new ByteArrayOutputStream();
+    try {
+      Method getInstance = ShowShipCommand.class.getDeclaredMethod("getInstance");
+      getInstance.setAccessible(true);
+      showShipCommand = (ShowShipCommand) getInstance.invoke(null);
+      execute = showShipCommand.getClass().getDeclaredMethod("executeCommand", String[].class);
+      execute.setAccessible(true);
+    } catch (InvocationTargetException | NoSuchMethodException | IllegalAccessException e) {
+      fail("Impossibile testare il comando /mostranavi");
+    }
+  }
+
+  @BeforeEach
+  void setUp() {
+    outContent.reset();
+    System.setOut(new PrintStream(outContent, true, StandardCharsets.UTF_8));
+  }
 }
