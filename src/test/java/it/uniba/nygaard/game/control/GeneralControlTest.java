d8696fd0c495a19039024024795dd191c4eb5ef6
Davide Cirilli <107247984+Davy592@users.noreply.github.com>
2023-06-13 00:23:30 +0200

    Aggiunti metodi @Before classe GeneralControlTest
    Co-authored-by: Mattia Curri <mattia2002@gmail.com>

--- src/test/java/it/uniba/nygaard/game/control/GeneralControlTest.java
+++ src/test/java/it/uniba/nygaard/game/control/GeneralControlTest.java
@@ -7,4 +7,27 @@ class GeneralControlTest {
   private static ByteArrayOutputStream outContent;
   private static InputStream inBackup;
 
+  @BeforeAll
+  static void setUpAll() {
+    outContent = new ByteArrayOutputStream();
+    inBackup = System.in;
+    try {
+      Method method = GeneralControl.class.getDeclaredMethod("getInstance");
+      method.setAccessible(true);
+      GeneralControl generalControl = (GeneralControl) method.invoke(null);
+      initCommands = generalControl.getClass().getDeclaredMethod("initCommands");
+      initCommands.setAccessible(true);
+      startGame = generalControl.getClass().getDeclaredMethod("startGame", String[].class);
+      startGame.setAccessible(true);
+    } catch (InvocationTargetException | NoSuchMethodException | IllegalAccessException e) {
+      fail("Impossibile testare la classe GeneralControl");
+    }
+  }
+
+  @BeforeEach
+  void setUp() {
+    outContent.reset();
+    System.setOut(new PrintStream(outContent, true, StandardCharsets.UTF_8));
+  }
+
 }
