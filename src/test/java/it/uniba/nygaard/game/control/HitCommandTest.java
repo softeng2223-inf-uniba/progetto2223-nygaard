499f3777d10a15470c07a63b186db98bb12e5431
Davide Cirilli <107247984+Davy592@users.noreply.github.com>
2023-06-12 23:32:12 +0200

    Aggiunti metodi @Before classe HitCommandTest
    Co-authored-by: Mattia Curri <mattia2002@gmail.com>

--- src/test/java/it/uniba/nygaard/game/control/HitCommandTest.java
+++ src/test/java/it/uniba/nygaard/game/control/HitCommandTest.java
@@ -6,4 +6,27 @@ class HitCommandTest {
   private static Method execute;
   private static ByteArrayOutputStream outContent;
   private static InputStream inBackup;
+
+  @BeforeAll
+  static void setUpAll() {
+    outContent = new ByteArrayOutputStream();
+    inBackup = System.in;
+    try {
+      Method getInstance = HitCommand.class.getDeclaredMethod("getInstance");
+      getInstance.setAccessible(true);
+      hitCommand = (HitCommand) getInstance.invoke(null);
+      execute = hitCommand.getClass().getDeclaredMethod("executeCommand", String[].class);
+      execute.setAccessible(true);
+    } catch (InvocationTargetException | NoSuchMethodException | IllegalAccessException e) {
+      fail("Impossibile testare il comando colpo");
+    }
+  }
+
+  @BeforeEach
+  void setUp() {
+    outContent.reset();
+    System.setOut(new PrintStream(outContent, true, StandardCharsets.UTF_8));
+    GeneralControl.setShutDown(UShutdown.NOT_TERMINATION_CODE);
+    GameManager.getMatch().setInGame(true);
+  }
 }
