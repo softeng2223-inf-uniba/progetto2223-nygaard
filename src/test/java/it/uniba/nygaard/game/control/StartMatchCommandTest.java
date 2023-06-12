826c117d6315b7f136dec0a6ee099c3a5f9b57a8
Davide Cirilli <107247984+Davy592@users.noreply.github.com>
2023-06-13 00:20:32 +0200

    Aggiunti attributi classe StartMatchCommandTest
    Co-authored-by: Mattia Curri <mattia2002@gmail.com>

--- src/test/java/it/uniba/nygaard/game/control/StartMatchCommandTest.java
+++ src/test/java/it/uniba/nygaard/game/control/StartMatchCommandTest.java
@@ -1,4 +1,9 @@
 package it.uniba.nygaard.game.control;
 
 class StartMatchCommandTest {
+
+  private static StartMatchCommand startMatchCommand;
+  private static Method execute;
+  private static ByteArrayOutputStream outContent;
+  
 }
