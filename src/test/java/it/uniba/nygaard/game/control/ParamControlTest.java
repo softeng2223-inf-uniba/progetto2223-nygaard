bbe39f2ab200fe599fc8728e97588c3a3c474733
Davide Cirilli <107247984+Davy592@users.noreply.github.com>
2023-06-12 23:41:36 +0200

    Creati metodi @Before classe ParamControlTest
    Co-authored-by: Mattia Curri <mattia2002@gmail.com>

--- src/test/java/it/uniba/nygaard/game/control/ParamControlTest.java
+++ src/test/java/it/uniba/nygaard/game/control/ParamControlTest.java
@@ -5,4 +5,22 @@ class ParamControlTest {
   private static ByteArrayOutputStream outContent;
   private static Method initUI;
 
+  @BeforeAll
+  static void setUpAll() {
+    try {
+      initUI = ParamControl.class.getDeclaredMethod("initUI");
+      initUI.setAccessible(true);
+    } catch (NoSuchMethodException e) {
+      fail("Impossibile testare la classe ParamControl");
+    }
+    outContent = new ByteArrayOutputStream();
+  }
+
+  @BeforeEach
+  void setUp() {
+    outContent.reset();
+    System.setOut(new PrintStream(outContent, true, StandardCharsets.UTF_8));
+    GeneralControl.setShutDown(UShutdown.NOT_TERMINATION_CODE);
+  }
+
 }
