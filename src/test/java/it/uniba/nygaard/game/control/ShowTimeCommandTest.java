7a5d01960fd8d746e2f1270c23c8597b98d7d32e
Davide Cirilli <107247984+Davy592@users.noreply.github.com>
2023-06-13 00:18:05 +0200

    Aggiunti attributi classe ShowTimeCommandTest
    Co-authored-by: Mattia Curri <mattia2002@gmail.com>

--- src/test/java/it/uniba/nygaard/game/control/ShowTimeCommandTest.java
+++ src/test/java/it/uniba/nygaard/game/control/ShowTimeCommandTest.java
@@ -1,4 +1,9 @@
 package it.uniba.nygaard.game.control;
 
 class ShowTimeCommandTest {
+
+  private static ShowTimeCommand showTimeCommand;
+  private static Method execute;
+  private static ByteArrayOutputStream outContent;
+
 }
