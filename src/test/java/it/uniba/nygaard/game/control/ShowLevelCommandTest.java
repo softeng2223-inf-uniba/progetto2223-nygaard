0ec62467ef497806dd8abe874953c2592d40e971
Davide Cirilli <107247984+Davy592@users.noreply.github.com>
2023-06-13 00:13:58 +0200

    Aggiunti metodi @Before classe ShowLevelCommandTest
    Co-authored-by: Mattia Curri <mattia2002@gmail.com>

--- src/test/java/it/uniba/nygaard/game/control/ShowLevelCommandTest.java
+++ src/test/java/it/uniba/nygaard/game/control/ShowLevelCommandTest.java
@@ -6,4 +6,25 @@ class ShowLevelCommandTest {
   private static Method execute;
   private static ByteArrayOutputStream outContent;
 
+  @BeforeAll
+  static void setUpAll() {
+    outContent = new ByteArrayOutputStream();
+    try {
+      GameManager.setMatch(new Match());
+      Method getInstance = ShowLevelCommand.class.getDeclaredMethod("getInstance");
+      getInstance.setAccessible(true);
+      showLevelCommand = (ShowLevelCommand) getInstance.invoke(null);
+      execute = showLevelCommand.getClass().getDeclaredMethod("executeCommand", String[].class);
+      execute.setAccessible(true);
+    } catch (InvocationTargetException | NoSuchMethodException | IllegalAccessException e) {
+      fail("Impossibile testare il comando /mostralivello");
+    }
+  }
+
+  @BeforeEach
+  void setUp() {
+    outContent.reset();
+    System.setOut(new PrintStream(outContent, true, StandardCharsets.UTF_8));
+  }
+
 }
