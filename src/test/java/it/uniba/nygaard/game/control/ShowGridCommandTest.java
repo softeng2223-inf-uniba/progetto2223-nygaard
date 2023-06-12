7b37a12ff091c8a2ad4be4d1fdfa0a928ecd5da9
Davide Cirilli <107247984+Davy592@users.noreply.github.com>
2023-06-13 00:11:36 +0200

    Aggiunti test /mostragriglia e /svelagriglia troppi argomenti
    Co-authored-by: Mattia Curri <mattia2002@gmail.com>

--- src/test/java/it/uniba/nygaard/game/control/ShowGridCommandTest.java
+++ src/test/java/it/uniba/nygaard/game/control/ShowGridCommandTest.java
@@ -28,4 +28,34 @@ class ShowGridCommandTest {
     GameManager.getMatch().setInGame(true);
   }
 
+  @Test
+  @DisplayName("/mostragriglia con troppi argomenti")
+  void testShowAttackGridCommandWithTooManyArgument() {
+    String[] args = new String[]{"/mostragriglia", "stringa"};
+    try {
+      execute.invoke(revealGridCommand, (Object) args);
+    } catch (Exception e) {
+      fail("Impossibile invocare il metodo executeCommand");
+    }
+    String expectedOutput = UColor.CYAN + "Il comando dovrebbe essere usato come segue: /mostragriglia" + UColor.RESET
+        + System.lineSeparator();
+    assertEquals(expectedOutput, outContent.toString(StandardCharsets.UTF_8),
+        "Non è stato stampato il messaggio di errore corretto");
+  }
+
+  @Test
+  @DisplayName("/svelagriglia con troppi argomenti")
+  void testShowDefenseGridCommandWithTooManyArgument() {
+    String[] args = new String[]{"/svelagriglia", "stringa"};
+    try {
+      execute.invoke(revealGridCommand, (Object) args);
+    } catch (Exception e) {
+      fail("Impossibile invocare il metodo executeCommand");
+    }
+    String expectedOutput = UColor.CYAN + "Il comando dovrebbe essere usato come segue: /svelagriglia" + UColor.RESET
+        + System.lineSeparator();
+    assertEquals(expectedOutput, outContent.toString(StandardCharsets.UTF_8),
+        "Non è stato stampato il messaggio di errore corretto");
+  }
+
 }
