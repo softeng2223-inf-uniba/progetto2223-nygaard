d8a2e1a7ae29c1349d90b1fb6087a761682f4c64
Davide Cirilli <107247984+Davy592@users.noreply.github.com>
2023-06-13 00:01:39 +0200

    Aggiunti attributi classe SetTimeCommandTest
    Co-authored-by: Mattia Curri <mattia2002@gmail.com>

--- src/test/java/it/uniba/nygaard/game/control/SetTimeCommandTest.java
+++ src/test/java/it/uniba/nygaard/game/control/SetTimeCommandTest.java
@@ -1,4 +1,9 @@
 package it.uniba.nygaard.game.control;
 
 class SetTimeCommandTest {
+
+  private static SetTimeCommand setTimeCommand;
+  private static Method execute;
+  private static ByteArrayOutputStream outContent;
+
 }
