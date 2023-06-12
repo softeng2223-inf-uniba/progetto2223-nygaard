a8e40a9e7a3946c5492c25f71a9d426d4c2940ed
Davide Cirilli <107247984+Davy592@users.noreply.github.com>
2023-06-12 23:25:49 +0200

    Aggiunto test /esci con troppi argomenti
    Co-authored-by: Mattia Curri <mattia2002@gmail.com>

--- src/test/java/it/uniba/nygaard/game/control/ExitCommandTest.java
+++ src/test/java/it/uniba/nygaard/game/control/ExitCommandTest.java
@@ -28,4 +28,19 @@ class ExitCommandTest {
     System.setOut(new PrintStream(outContent, true, StandardCharsets.UTF_8));
     GeneralControl.setShutDown(UShutdown.NOT_TERMINATION_CODE);
   }
+
+  @Test
+  @DisplayName("/esci con troppi argomenti")
+  void testExitCommandWithTooManyArgument() {
+    String[] args = new String[]{"/esci", "stringa", "inutile"};
+    try {
+      execute.invoke(exitCommand, (Object) args);
+    } catch (IllegalAccessException | InvocationTargetException e) {
+      fail("Impossibile invocare il metodo executeCommand");
+    }
+    String expectedOutput = UColor.CYAN + "Il comando dovrebbe essere usato come segue: "
+        + "/esci" + UColor.RESET + System.lineSeparator();
+    assertEquals(expectedOutput, outContent.toString(StandardCharsets.UTF_8),
+        "Non è stato stampato il messaggio di errore corretto");
+  }
 }
