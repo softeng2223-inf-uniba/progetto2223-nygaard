f33db3b5d6ca61e50457a6e8259a0f04db407289
Davide Cirilli <107247984+Davy592@users.noreply.github.com>
2023-06-13 00:32:03 +0200

    Aggiunti metodi @Before classe ShipTest
    Co-authored-by: Mattia Curri <mattia2002@gmail.com>

--- src/test/java/it/uniba/nygaard/game/entity/ships/ShipTest.java
+++ src/test/java/it/uniba/nygaard/game/entity/ships/ShipTest.java
@@ -7,4 +7,17 @@ class ShipTest {
   private static Coordinate coord;
   private static Random rnd;
 
+  @BeforeAll
+  static void setUpAll() {
+    coord = new Coordinate();
+    rnd = new Random();
+  }
+
+  @BeforeEach
+  void setUp() {
+    ship = new Ship(rnd.nextInt(UShip.BATTLESHIP_HP) + UShip.DESTROYER_HP);
+    grid = new CellsGrid(UGrid.STANDARD_GRID_SIZE);
+    rnd.setSeed(System.currentTimeMillis());
+  }
+
 }
