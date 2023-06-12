6b240deeebaa75e715f5e4a8d79f20eb43166adc
Davide Cirilli <107247984+Davy592@users.noreply.github.com>
2023-06-13 00:15:49 +0200

    Aggiunti attributi classe ShowShipCommandTest
    Co-authored-by: Mattia Curri <mattia2002@gmail.com>

--- src/test/java/it/uniba/nygaard/game/control/ShowShipCommandTest.java
+++ src/test/java/it/uniba/nygaard/game/control/ShowShipCommandTest.java
@@ -1,4 +1,9 @@
 package it.uniba.nygaard.game.control;
 
 class ShowShipCommandTest {
+
+  private static ShowShipCommand showShipCommand;
+  private static Method execute;
+  private static ByteArrayOutputStream outContent;
+
 }
