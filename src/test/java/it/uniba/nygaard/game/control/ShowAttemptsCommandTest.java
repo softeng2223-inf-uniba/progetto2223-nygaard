c83a21640fa0e95ed19ea82a9abf01db432a5f44
Davide Cirilli <107247984+Davy592@users.noreply.github.com>
2023-06-13 00:06:28 +0200

    Aggiunti attributi classe ShowAttemptsCommandTest
    Co-authored-by: Mattia Curri <mattia2002@gmail.com>

--- src/test/java/it/uniba/nygaard/game/control/ShowAttemptsCommandTest.java
+++ src/test/java/it/uniba/nygaard/game/control/ShowAttemptsCommandTest.java
@@ -1,4 +1,9 @@
 package it.uniba.nygaard.game.control;
 
 class ShowAttemptsCommandTest {
+
+  private static ShowAttemptsCommand showAttemptsCommand;
+  private static Method execute;
+  private static ByteArrayOutputStream outContent;
+
 }
