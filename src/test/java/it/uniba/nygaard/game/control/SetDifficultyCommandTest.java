4fd6b34c3712aec2f8622d87517f0df9974ca3bd
Davide Cirilli <107247984+Davy592@users.noreply.github.com>
2023-06-12 23:46:08 +0200

    Aggiunti metodi @Before classe SetDifficultyCommandTest
    Co-authored-by: Mattia Curri <mattia2002@gmail.com>

--- src/test/java/it/uniba/nygaard/game/control/SetDifficultyCommandTest.java
+++ src/test/java/it/uniba/nygaard/game/control/SetDifficultyCommandTest.java
@@ -7,4 +7,28 @@ class SetDifficultyCommandTest {
   private static ByteArrayOutputStream outContent;
   private static InputStream inBackup;
 
+
+  @BeforeAll
+  static void setUpAll() {
+    outContent = new ByteArrayOutputStream();
+    inBackup = System.in;
+    try {
+      GameManager.setMatch(new Match());
+      Method getInstance = SetDifficultyCommand.class.getDeclaredMethod("getInstance");
+      getInstance.setAccessible(true);
+      setDifficultyCommand = (SetDifficultyCommand) getInstance.invoke(null);
+      execute = setDifficultyCommand.getClass().getDeclaredMethod("executeCommand", String[].class);
+      execute.setAccessible(true);
+    } catch (InvocationTargetException | NoSuchMethodException | IllegalAccessException e) {
+      fail("Impossibile testare i comandi /facile, /medio e /difficile");
+    }
+  }
+
+  @BeforeEach
+  void setUp() {
+    outContent.reset();
+    System.setOut(new PrintStream(outContent, true, StandardCharsets.UTF_8));
+    GameManager.getMatch().setDifficulty(UDifficulty.DIFFICULTY_MEDIUM);
+    GameManager.getMatch().setInGame(false);
+  }
 }
