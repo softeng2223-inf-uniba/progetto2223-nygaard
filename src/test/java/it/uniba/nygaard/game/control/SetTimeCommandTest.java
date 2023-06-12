df6d66071d2e33fd3f121507fcaf27566e68cdf9
Davide Cirilli <107247984+Davy592@users.noreply.github.com>
2023-06-13 00:02:47 +0200

    Aggiunto test /tempo senza argomenti
    Co-authored-by: Mattia Curri <mattia2002@gmail.com>

--- src/test/java/it/uniba/nygaard/game/control/SetTimeCommandTest.java
+++ src/test/java/it/uniba/nygaard/game/control/SetTimeCommandTest.java
@@ -27,4 +27,19 @@ class SetTimeCommandTest {
     System.setOut(new PrintStream(outContent, true, StandardCharsets.UTF_8));
     GameManager.getMatch().setInGame(false);
   }
+
+  @Test
+  @DisplayName("/tempo senza argomenti")
+  void testSetTimeCommandWithoutArgument() {
+    String[] args = new String[]{"/tempo"};
+    try {
+      execute.invoke(setTimeCommand, (Object) args);
+    } catch (IllegalAccessException | InvocationTargetException e) {
+      fail("Impossibile invocare il metodo executeCommand");
+    }
+    String expectedOutput = UColor.CYAN + "Il comando dovrebbe essere usato come segue: "
+        + "/tempo <tempo in minuti>" + UColor.RESET + System.lineSeparator();
+    assertEquals(expectedOutput, outContent.toString(StandardCharsets.UTF_8),
+        "Non è stato stampato il messaggio di errore corretto");
+  }
 }
