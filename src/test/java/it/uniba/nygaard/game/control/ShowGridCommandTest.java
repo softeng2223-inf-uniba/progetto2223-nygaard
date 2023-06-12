c553a8132773209efd83f814d748e28db7b64eb2
Davide Cirilli <107247984+Davy592@users.noreply.github.com>
2023-06-13 00:10:27 +0200

    Aggiunti attributi classe ShowGridCommandTest
    Co-authored-by: Mattia Curri <mattia2002@gmail.com>

--- src/test/java/it/uniba/nygaard/game/control/ShowGridCommandTest.java
+++ src/test/java/it/uniba/nygaard/game/control/ShowGridCommandTest.java
@@ -1,4 +1,9 @@
 package it.uniba.nygaard.game.control;
 
 class ShowGridCommandTest {
+
+  private static ShowGridCommand revealGridCommand;
+  private static Method execute;
+  private static ByteArrayOutputStream outContent;
+
 }
