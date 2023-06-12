00e0163fb20c51064b52cd3df918f8fd0fba5380
Davide Cirilli <107247984+Davy592@users.noreply.github.com>
2023-06-12 23:56:58 +0200

    Aggiunti metodi @Before classe SetGridSizeCommandTest
    Co-authored-by: Mattia Curri <mattia2002@gmail.com>

--- src/test/java/it/uniba/nygaard/game/control/SetGridSizeCommandTest.java
+++ src/test/java/it/uniba/nygaard/game/control/SetGridSizeCommandTest.java
@@ -6,5 +6,28 @@ class SetGridSizeCommandTest {
   private static Method execute;
   private static ByteArrayOutputStream outContent;
   private static InputStream inBackup;
-  
+
+  @BeforeAll
+  static void setUpAll() {
+    outContent = new ByteArrayOutputStream();
+    inBackup = System.in;
+    try {
+      GameManager.setMatch(new Match());
+      Method getInstance = SetGridSizeCommand.class.getDeclaredMethod("getInstance");
+      getInstance.setAccessible(true);
+      setGridSizeCommand = (SetGridSizeCommand) getInstance.invoke(null);
+      execute = setGridSizeCommand.getClass().getDeclaredMethod("executeCommand", String[].class);
+      execute.setAccessible(true);
+    } catch (InvocationTargetException | NoSuchMethodException | IllegalAccessException e) {
+      fail("Impossibile testare i comandi /standard, /large ed /extralarge");
+    }
+  }
+
+  @BeforeEach
+  void setUp() {
+    outContent.reset();
+    System.setOut(new PrintStream(outContent, true, StandardCharsets.UTF_8));
+    GameManager.getMatch().setGridSize(UGrid.STANDARD_GRID_SIZE);
+    GameManager.getMatch().setInGame(false);
+  }
 }
