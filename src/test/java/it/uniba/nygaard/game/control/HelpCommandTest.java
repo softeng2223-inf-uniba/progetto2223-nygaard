c35347899bbb2df0a1e290cc949854c01107b6d9
Davide Cirilli <107247984+Davy592@users.noreply.github.com>
2023-06-12 23:29:20 +0200

    Aggiunti attributi classe HelpCommandTest
    Co-authored-by: Mattia Curri <mattia2002@gmail.com>

--- src/test/java/it/uniba/nygaard/game/control/HelpCommandTest.java
+++ src/test/java/it/uniba/nygaard/game/control/HelpCommandTest.java
@@ -1,4 +1,9 @@
 package it.uniba.nygaard.game.control;
 
 class HelpCommandTest {
+
+  private static HelpCommand helpCommand;
+  private static Method execute;
+  private static ByteArrayOutputStream outContent;
+
 }
