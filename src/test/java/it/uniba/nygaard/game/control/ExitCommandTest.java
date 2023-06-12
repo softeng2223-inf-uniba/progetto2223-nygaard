78f582dc76db558e0b8b375689a2e1987abd0408
Davide Cirilli <107247984+Davy592@users.noreply.github.com>
2023-06-12 23:25:12 +0200

    Aggiunti metodi @Before classe ExitCommandTest
    Co-authored-by: Mattia Curri <mattia2002@gmail.com>

--- src/test/java/it/uniba/nygaard/game/control/ExitCommandTest.java
+++ src/test/java/it/uniba/nygaard/game/control/ExitCommandTest.java
@@ -6,5 +6,26 @@ class ExitCommandTest {
   private static Method execute;
   private static ByteArrayOutputStream outContent;
   private static InputStream inBackup;
-  
+
+  @BeforeAll
+  static void setUpAll() {
+    outContent = new ByteArrayOutputStream();
+    inBackup = System.in;
+    try {
+      Method getInstance = ExitCommand.class.getDeclaredMethod("getInstance");
+      getInstance.setAccessible(true);
+      exitCommand = (ExitCommand) getInstance.invoke(null);
+      execute = exitCommand.getClass().getDeclaredMethod("executeCommand", String[].class);
+      execute.setAccessible(true);
+    } catch (InvocationTargetException | NoSuchMethodException | IllegalAccessException e) {
+      fail("Impossibile testare il comando /esci");
+    }
+  }
+
+  @BeforeEach
+  void setUp() {
+    outContent.reset();
+    System.setOut(new PrintStream(outContent, true, StandardCharsets.UTF_8));
+    GeneralControl.setShutDown(UShutdown.NOT_TERMINATION_CODE);
+  }
 }
