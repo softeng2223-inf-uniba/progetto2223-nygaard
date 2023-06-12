568a713d4c5ad048d2fb7cd3b3c11e16ba1efcf5
Davide Cirilli <107247984+Davy592@users.noreply.github.com>
2023-06-12 23:24:45 +0200

    Aggiunti attributi classe ExitCommandTest
    Co-authored-by: Mattia Curri <mattia2002@gmail.com>

--- src/test/java/it/uniba/nygaard/game/control/ExitCommandTest.java
+++ src/test/java/it/uniba/nygaard/game/control/ExitCommandTest.java
@@ -1,4 +1,10 @@
 package it.uniba.nygaard.game.control;
 
 class ExitCommandTest {
+
+  private static ExitCommand exitCommand;
+  private static Method execute;
+  private static ByteArrayOutputStream outContent;
+  private static InputStream inBackup;
+  
 }
