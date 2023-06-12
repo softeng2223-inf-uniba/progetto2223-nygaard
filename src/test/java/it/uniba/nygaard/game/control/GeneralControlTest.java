39cefad0c5fcc6a9c8ae3d50f06c8fc204b738b6
Davide Cirilli <107247984+Davy592@users.noreply.github.com>
2023-06-13 00:23:11 +0200

    Aggiunti attributi classe GeneralControlTest
    Co-authored-by: Mattia Curri <mattia2002@gmail.com>

--- src/test/java/it/uniba/nygaard/game/control/GeneralControlTest.java
+++ src/test/java/it/uniba/nygaard/game/control/GeneralControlTest.java
@@ -1,4 +1,10 @@
 package it.uniba.nygaard.game.control;
 
 class GeneralControlTest {
+
+  private static Method initCommands;
+  private static Method startGame;
+  private static ByteArrayOutputStream outContent;
+  private static InputStream inBackup;
+
 }
