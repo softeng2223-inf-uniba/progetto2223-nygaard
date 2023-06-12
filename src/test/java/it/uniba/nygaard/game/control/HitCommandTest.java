531f026c05cddcff085143459600a56eedd78d21
Davide Cirilli <107247984+Davy592@users.noreply.github.com>
2023-06-12 23:31:45 +0200

    Aggiunti attributi classe HitCommandTest
    Co-authored-by: Mattia Curri <mattia2002@gmail.com>

--- src/test/java/it/uniba/nygaard/game/control/HitCommandTest.java
+++ src/test/java/it/uniba/nygaard/game/control/HitCommandTest.java
@@ -1,4 +1,9 @@
 package it.uniba.nygaard.game.control;
 
 class HitCommandTest {
+
+  private static HitCommand hitCommand;
+  private static Method execute;
+  private static ByteArrayOutputStream outContent;
+  private static InputStream inBackup;
 }
