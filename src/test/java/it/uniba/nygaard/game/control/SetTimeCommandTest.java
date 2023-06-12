7f91dd3ad488ac9d77f66b6b51cefa09720e9c8d
Davide Cirilli <107247984+Davy592@users.noreply.github.com>
2023-06-13 00:02:20 +0200

    Aggiunti metodi @Before classe SetTimeCommandTest
    Co-authored-by: Mattia Curri <mattia2002@gmail.com>

--- src/test/java/it/uniba/nygaard/game/control/SetTimeCommandTest.java
+++ src/test/java/it/uniba/nygaard/game/control/SetTimeCommandTest.java
@@ -6,4 +6,25 @@ class SetTimeCommandTest {
   private static Method execute;
   private static ByteArrayOutputStream outContent;
 
+  @BeforeAll
+  static void setUpAll() {
+    outContent = new ByteArrayOutputStream();
+    try {
+      GameManager.setMatch(new Match());
+      Method getInstance = SetTimeCommand.class.getDeclaredMethod("getInstance");
+      getInstance.setAccessible(true);
+      setTimeCommand = (SetTimeCommand) getInstance.invoke(null);
+      execute = setTimeCommand.getClass().getDeclaredMethod("executeCommand", String[].class);
+      execute.setAccessible(true);
+    } catch (InvocationTargetException | NoSuchMethodException | IllegalAccessException e) {
+      fail("Impossibile testare il comando /tempo");
+    }
+  }
+
+  @BeforeEach
+  void setUp() {
+    outContent.reset();
+    System.setOut(new PrintStream(outContent, true, StandardCharsets.UTF_8));
+    GameManager.getMatch().setInGame(false);
+  }
 }
