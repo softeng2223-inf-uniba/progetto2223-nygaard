1629d55f02f3f230c528b0beae7c9ef069ed021d
Davide Cirilli <107247984+Davy592@users.noreply.github.com>
2023-06-13 00:20:58 +0200

    Aggiunti metodi @Before classe StartMatchCommandTest
    Co-authored-by: Mattia Curri <mattia2002@gmail.com>

--- src/test/java/it/uniba/nygaard/game/control/StartMatchCommandTest.java
+++ src/test/java/it/uniba/nygaard/game/control/StartMatchCommandTest.java
@@ -5,5 +5,25 @@ class StartMatchCommandTest {
   private static StartMatchCommand startMatchCommand;
   private static Method execute;
   private static ByteArrayOutputStream outContent;
-  
+
+  @BeforeAll
+  static void setUpAll() {
+    outContent = new ByteArrayOutputStream();
+    try {
+      Method getInstance = StartMatchCommand.class.getDeclaredMethod("getInstance");
+      getInstance.setAccessible(true);
+      startMatchCommand = (StartMatchCommand) getInstance.invoke(null);
+      execute = startMatchCommand.getClass().getDeclaredMethod("executeCommand", String[].class);
+      execute.setAccessible(true);
+    } catch (InvocationTargetException | NoSuchMethodException | IllegalAccessException e) {
+      fail("Impossibile testare il comando /gioca");
+    }
+  }
+
+  @BeforeEach
+  void setUp() {
+    outContent.reset();
+    System.setOut(new PrintStream(outContent, true, StandardCharsets.UTF_8));
+    GameManager.getMatch().setInGame(false);
+  }
 }
