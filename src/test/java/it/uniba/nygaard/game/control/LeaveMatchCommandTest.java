6e2b962cdee19e879b5df4a2200bed9441210d73
Davide Cirilli <107247984+Davy592@users.noreply.github.com>
2023-06-12 23:36:35 +0200

    Creati metodi @Before classe LeaveMatchCommandTest
    Co-authored-by: Mattia Curri <mattia2002@gmail.com>

--- src/test/java/it/uniba/nygaard/game/control/LeaveMatchCommandTest.java
+++ src/test/java/it/uniba/nygaard/game/control/LeaveMatchCommandTest.java
@@ -6,4 +6,26 @@ class LeaveMatchCommandTest {
   private static Method execute;
   private static ByteArrayOutputStream outContent;
   private static InputStream inBackup;
+
+  @BeforeAll
+  static void setUpAll() {
+    outContent = new ByteArrayOutputStream();
+    inBackup = System.in;
+    try {
+      Method getInstance = LeaveMatchCommand.class.getDeclaredMethod("getInstance");
+      getInstance.setAccessible(true);
+      leaveMatchCommand = (LeaveMatchCommand) getInstance.invoke(null);
+      execute = leaveMatchCommand.getClass().getDeclaredMethod("executeCommand", String[].class);
+      execute.setAccessible(true);
+    } catch (InvocationTargetException | NoSuchMethodException | IllegalAccessException e) {
+      fail("Impossibile testare il comando /abbandona");
+    }
+  }
+
+  @BeforeEach
+  void setUp() {
+    outContent.reset();
+    System.setOut(new PrintStream(outContent, true, StandardCharsets.UTF_8));
+    GeneralControl.setShutDown(UShutdown.NOT_TERMINATION_CODE);
+  }
 }
