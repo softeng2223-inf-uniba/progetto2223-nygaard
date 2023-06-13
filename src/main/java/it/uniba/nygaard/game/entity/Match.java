59e906c9534a6afca785deb5e5ad9f4e046f82fd
GiudittaIzzo <izzogiuditta.2e@gmail.com>
2023-06-13 18:14:11 +0200

    Aggiunto JavaDoc JavaDoc di hit in Match

--- src/main/java/it/uniba/nygaard/game/entity/Match.java
+++ src/main/java/it/uniba/nygaard/game/entity/Match.java
@@ -464,7 +464,7 @@ public final class Match {
   /**
    * <h3> hit </h3>
    * <p>
-   * Il metodo hit colpisce la cella di coordinate (row, column).
+   * Il metodo hit colpisce la cella di coordinate (row, column) e restituisce cosa è stato colpito
    * </p>
    *
    * @param row    Riga della cella da colpire.
